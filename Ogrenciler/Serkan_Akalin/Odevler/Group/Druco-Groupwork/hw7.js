e00ba1bb22b7537fd662dc1015960ed02bfedce4
macroumix <99314240+macroumix@users.noreply.github.com>
2023-03-22 21:13:17 +0300

    merge html-css

---
+++ Ogrenciler/Serkan_Akalin/Odevler/Group/Druco-Groupwork/hw7.js
@@ -0,0 +1,44 @@
+function displayHome() {
+    var x = document.getElementById("nav-subpart-home");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function displayPages() {
+    var x = document.getElementById("nav-subpart-pages");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function displayServiceParts() {
+    var x = document.getElementById("nav-subpart-serviceparts");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function displayBrake() {
+    var x = document.getElementById("nav-subpart-brake");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function displayCarBatteries() {
+    var x = document.getElementById("nav-subpart-carbatteries");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
