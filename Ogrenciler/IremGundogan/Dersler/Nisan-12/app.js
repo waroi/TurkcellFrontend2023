2b01331f53834a408f138ce1c1b1d22061531cd4
İrem Gündoğan <rem1035@hotmail.com>
2023-04-12 23:24:01 +0300

    not

---
+++ Ogrenciler/IremGundogan/Dersler/Nisan-12/app.js
@@ -0,0 +1,25 @@
+/*js giriş*/
+console.log("Merhaba Javascript");
+
+//>Değişkenler
+// Değiken tanımlama
+
+// var firstNumber = 10;
+
+// secondNumber = 20;
+
+// console.log(firstNumber);
+// console.log(secondNumber);
+// console.log(firstNumber + secondNumber);
+
+//*bak
+var secondNumber = 20;
+
+a()
+function a() {
+  firstNumber = 10;
+}
+
+console.log(firstNumber);
+console.log(secondNumber);
+console.log(firstNumber + secondNumber);
