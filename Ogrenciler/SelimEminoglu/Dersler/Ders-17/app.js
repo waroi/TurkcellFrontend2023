c8b48f983ff911ee14d8cd102f16dc083ed2f042
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-04-12 21:28:59 +0300

    Ders İçeriği Eklendi
    Ders-17 eklendi

---
+++ Ogrenciler/SelimEminoglu/Dersler/Ders-17/app.js
@@ -0,0 +1,28 @@
+// Js Giriş
+console.log("merhaba dünya");
+
+// Değişkenler
+// Değişken tanımlama
+
+// genelde camelCase kullanılır
+
+// secondNumber =20;
+// firstNumber =18;
+
+// console.log(firstNumber);
+// console.log(secondNumber);
+
+// console.log(firstNumber+secondNumber);
+
+// var ile bloklar arasına hapsetme durumu olması
+var secondNumber =20;
+
+a()
+function a(){
+    firstNumber=12;
+}
+
+console.log(firstNumber);
+console.log(secondNumber);
+
+console.log(firstNumber+secondNumber);
