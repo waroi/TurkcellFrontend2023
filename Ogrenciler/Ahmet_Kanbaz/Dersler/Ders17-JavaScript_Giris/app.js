124b09cf8ea51c79e7c32d1bed3b2ea4797632f6
smartbreath <ahmetkanbaz@gmail.com>
2023-04-13 23:01:39 +0300

    Ders17-JavaScript-Giris

---
+++ Ogrenciler/Ahmet_Kanbaz/Dersler/Ders17-JavaScript_Giris/app.js
@@ -0,0 +1,16 @@
+document.write("App.js'den gelen deneme yazısı...")
+document.write("<br>2. Deneme")
+
+var firstNumber = 10;
+secondNumber = 20;
+console.log(firstNumber, secondNumber, firstNumber + secondNumber);
+
+var thirdNumber = 100;
+
+fourthNumber();
+
+function fourthNumber() {
+  anotherNumber = 200;
+}
+
+console.log(thirdNumber, anotherNumber, thirdNumber + anotherNumber);
