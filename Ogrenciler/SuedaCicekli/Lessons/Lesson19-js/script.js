7faa1cdca658e8b374578308357c46c90f9fc988
Sueda Çiçekli <suedacicekli@gmail.com>
2023-04-17 21:31:56 +0300

    add | lesson19

---
+++ Ogrenciler/SuedaCicekli/Lessons/Lesson19-js/script.js
@@ -0,0 +1,47 @@
+//Stirng Methodları
+
+let value;
+const firstName = "Sueda";
+const lastName = "Cicekli";
+const age = 24;
+const department = "Bilişim";
+
+// value = firstName + "" + lastName;
+// value = firstName;
+// value += "" + lastName;
+
+// value = firstName.length;
+// value = firstName.toLowerCase();
+// value = firstName.toUpperCase();
+// value = firstName[0];
+// value = firstName[firstName.length - 1];
+// value = firstName[-1];   //undefined
+// value = firstName.indexOf("a");
+// value = firstName.includes("e");
+
+//Template Literals
+// value = "My name is " + firstName + " " + lastName + " and I am " + age;
+// value = `
+// isim: ${firstName}
+// soyisim:${lastName}
+// Yaş: ${age}
+// `;
+// value = `${firstName} ${lastName} ${age}`;
+
+value = `
+<ul>
+<li>isim: ${firstName}</li>
+<li>soyisim: ${lastName}</li>
+<li>Yaş: ${age}</li>
+<li>Yaş: ${department}</li>
+</ul>
+`;
+document.body.innerHTML = value;
+
+
+
+
+
+
+
+console.log(value);
