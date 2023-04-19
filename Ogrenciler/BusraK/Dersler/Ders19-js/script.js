4cd94bed3cf3f7f45020031155fde4827132dc19
busra4 <bkosnak10@hotmail.com>
2023-04-19 04:19:44 +0300

    ders calisma

---
+++ Ogrenciler/BusraK/Dersler/Ders19-js/script.js
@@ -0,0 +1,43 @@
+//string methodlarii
+
+let value;
+const firstName = "busra";
+const lastName = "kosnak";
+const age = 22;
+const department = "Bilişim";
+//value = firstNme + " " + lastname;
+
+// value = firstName;
+// value += " " + lastName;
+hgjjhjk;
+=
+?)8665
+
+// value = firstName.length; //uzunlugu
+// value = firstName.toLowerCase(); //butun harfler kucuk
+// value = firstName.toUpperCase(); //butun harfler buyuk
+// value = firstName[0]; //index degerleri sifirdan baslar ve b yi gosterir
+// value = firstName[firstName.length - 1]; //son harfi verir
+value = firstName[-1];
+value = firstName.indexOf("o"); //o harfini bulur
+value = firstName.includes("a");
+value = firstName.contains("a");
+value = [firstName, lastName, age];
+value = firstName.concat(" ", lastName, " ", age); //birlestirme
+// value =
+//   "İsim: " +
+//   firstName +
+//   " " +
+//   "Soyisim: " +
+//   " " +
+//   lastName +
+//   " " +
+//   " Yaş: " +
+//   age +
+//   " " +
+//   "Bölüm: " +
+//   department;  //basina \n koyarsan alt alta
+
+//dersi tamamlamadim dinledim devami yazilacak
+
+console.log(value);
