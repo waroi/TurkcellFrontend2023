043eb00a8322d4ccc79c4ff561d16e7b9887f90d
07kagan07 <kagancoskun32@gmail.com>
2023-04-19 21:50:54 +0300

    score update

--- Ogrenciler/KaganCoskun/GrupCalismalari/Ders20-SayiTahmini/app.js
+++ Ogrenciler/KaganCoskun/GrupCalismalari/Ders20-SayiTahmini/app.js
@@ -1,71 +1,72 @@
-//     let randomNumber = Math.floor(Math.random() * 100) + 1;
-//     console.log(randomNumber)
-//     let counter =0;
-//     let tahmin;
-//     let score = [100,75,50,25]
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    console.log(randomNumber)
+    let counter =0;
+    let tahmin;
+    let score;
 
-//     for(let i=0; randomNumber != tahmin ;i++){
-//         counter++;
-//         tahmin = prompt("Sayı tahmin ediniz!")
+    for(let i=0; randomNumber != tahmin ;i++){
+        score=100-(5*counter)
+        counter++;
+        tahmin = prompt("Sayı tahmin ediniz!")
 
-//         if(tahmin>randomNumber){
-//             console.log("Aşağı")
-//         }
-//         else if(tahmin<randomNumber){
-//             console.log("yukarı")
-//         }
+        if(tahmin>randomNumber){
+            console.log("Aşağı")
+        }
+        else if(tahmin<randomNumber){
+            console.log("yukarı")
+        }
+    }
+
+    console.log(`Tebrikler ${counter} defada bildiniz :D Skorunuz ${score}`)
+
+    // if (counter <2){
+    //     console.log("Skorunuz",score[0])
+    // }
+    // else if(counter < 5){
+    //     console.log("Skorunuz :",score[1])
+    // }
+    // else if(counter < 7){
+    //     console.log("Skorunuz : " ,score[2])
+    // }
+    // else{
+    //     console.log("Skorunuz :",score[3])
+    // }
+
+// let randomNumber = Math.floor(Math.random() * 100) + 1;
+// let counter =0;
+// let score = [100,75,50,25]
+// console.log(randomNumber)
+
+
+// const startGame =()=>{
+//     counter++;
+//     let tahmin =document.getElementById("guessInput").value;
+//     document.getElementById("guessCount").innerHTML = `Tahmin Sayınız : ${counter}`
+    
+//     if(randomNumber==tahmin){
+//         let score = getScore()
+//         document.getElementById("upDown").innerHTML = `Tebrikler ${counter} defada bildiniz :D Skorunuz ${score} `
 //     }
+//     else if(tahmin>randomNumber){
+//             document.getElementById("upDown").innerHTML = "Aşağı"
+//     }
+//     else if(tahmin<randomNumber){
+//         document.getElementById("upDown").innerHTML = "Yukarı"
+//     }
+  
+// }
 
-//     console.log(`Tebrikler ${counter} defada bildiniz :D`)
-
+// const getScore = ()=>{
 //     if (counter <2){
-//         console.log("Skorunuz",score[0])
+//         return ("Skorunuz",score[0])
 //     }
 //     else if(counter < 5){
-//         console.log("Skorunuz :",score[1])
+//         return ("Skorunuz :",score[1])
 //     }
 //     else if(counter < 7){
-//         console.log("Skorunuz : " ,score[2])
+//         return("Skorunuz : " ,score[2])
 //     }
 //     else{
-//         console.log("Skorunuz :",score[3])
+//         return ("Skorunuz :",score[3])
 //     }
-
-let randomNumber = Math.floor(Math.random() * 100) + 1;
-let counter =0;
-let score = [100,75,50,25]
-console.log(randomNumber)
-
-
-const startGame =()=>{
-    counter++;
-    let tahmin =document.getElementById("guessInput").value;
-    document.getElementById("guessCount").innerHTML = `Tahmin Sayınız : ${counter}`
-    
-    if(randomNumber==tahmin){
-        let score = getScore()
-        document.getElementById("upDown").innerHTML = `Tebrikler ${counter} defada bildiniz :D Skorunuz ${score} `
-    }
-    else if(tahmin>randomNumber){
-            document.getElementById("upDown").innerHTML = "Aşağı"
-    }
-    else if(tahmin<randomNumber){
-        document.getElementById("upDown").innerHTML = "Yukarı"
-    }
-  
-}
-
-const getScore = ()=>{
-    if (counter <2){
-        return ("Skorunuz",score[0])
-    }
-    else if(counter < 5){
-        return ("Skorunuz :",score[1])
-    }
-    else if(counter < 7){
-        return("Skorunuz : " ,score[2])
-    }
-    else{
-        return ("Skorunuz :",score[3])
-    }
-  }
+//   }
