94845d84a9227ba8decb52b442b9a3f850ff48e3
busra4 <bkosnak10@hotmail.com>
2023-04-19 18:07:15 +0300

    ders tamamlandi

--- Ogrenciler/BusraK/Dersler/Ders19-js/script.js
+++ Ogrenciler/BusraK/Dersler/Ders19-js/script.js
@@ -4,26 +4,23 @@ let value;
 const firstName = "busra";
 const lastName = "kosnak";
 const age = 22;
-const department = "Bilişim";
+const department = "Tasarim";
 //value = firstNme + " " + lastname;
 
 // value = firstName;
 // value += " " + lastName;
-hgjjhjk;
-=
-?)8665
 
 // value = firstName.length; //uzunlugu
 // value = firstName.toLowerCase(); //butun harfler kucuk
 // value = firstName.toUpperCase(); //butun harfler buyuk
 // value = firstName[0]; //index degerleri sifirdan baslar ve b yi gosterir
 // value = firstName[firstName.length - 1]; //son harfi verir
-value = firstName[-1];
-value = firstName.indexOf("o"); //o harfini bulur
-value = firstName.includes("a");
-value = firstName.contains("a");
-value = [firstName, lastName, age];
-value = firstName.concat(" ", lastName, " ", age); //birlestirme
+// value = firstName[-1];
+// value = firstName.indexOf("o"); //o harfini bulur
+// value = firstName.includes("a");
+// value = firstName.contains("a");
+// value = [firstName, lastName, age];
+// value = firstName.concat(" ", lastName, " ", age); //birlestirme
 // value =
 //   "İsim: " +
 //   firstName +
@@ -38,6 +35,25 @@ value = firstName.concat(" ", lastName, " ", age); //birlestirme
 //   "Bölüm: " +
 //   department;  //basina \n koyarsan alt alta
 
-//dersi tamamlamadim dinledim devami yazilacak
+// Template Literal
+// value = `İsim: ${firstName}
+// Soyisim: ${lastName}
+// Yaş: ${age}
+// Bölüm: ${department}
+// `;
+// value = `${firstName} ${lastName} ${age} ${department}`;
+
+value = `
+<ul>
+  <li>İsminiz: ${firstName}</li>
+  <li>Soyisminiz: ${lastName}</li>
+  <li>Yaşiniz: ${age}</li>
+  <li>Bölümünüz: ${department}</li>
+</ul>
+`;
+
+document.body.innerHTML = value;
+
+console.log(value);
 
 console.log(value);
