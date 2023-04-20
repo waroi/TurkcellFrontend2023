313b61f09e8b7d9de7f207525acf7ef9dc7597d4
smartbreath <ahmetkanbaz@gmail.com>
2023-04-20 03:48:18 +0300

    Ders20-JavaScript-Calismalar

---
+++ Ogrenciler/Ahmet_Kanbaz/Dersler/Ders18-JavaScript-Degiskenler/script.js
@@ -0,0 +1,56 @@
+//Variables - Değişkenler
+
+//Number - Sayısal Değerler
+// var number1 = 10;
+// console.log(number1);
+// console.log(number1 + 10);
+// console.log(typeof number1);
+
+//String - Metinsel Değerler
+// var userName = "Ahmet KANBAZ";
+// console.log(userName);
+// console.log(typeof userName);
+
+//Boolean - Mantıksal Değerler
+// var isOnline = true;
+// console.log(isOnline);
+// console.log(typeof isOnline);
+// var notOnline = false;
+// console.log(notOnline);
+// console.log(typeof notOnline);
+
+//Undefined - Tanımsız Değerler
+// var isUndefined;
+// console.log(isUndefined);
+// console.log(typeof isUndefined);
+
+//Null - Boş Değerler
+// var isNull = null;
+// console.log(isNull);
+// console.log(typeof isNull);
+
+//Object - Nesne Değerleri
+// var user = {
+//   userName: "Ahmet KANBAZ",
+//   age: 24,
+//   isOnline: true,
+// }
+// console.log(user);
+// console.log(typeof user);
+// console.log(user.isOnline);
+
+//Array - Dizi Değerleri
+// var arrayDeneme = [10, 40, "Ahmet", "KANBAZ", true, false, null, undefined];
+// console.log(arrayDeneme);
+// console.log(typeof arrayDeneme);
+// console.log(arrayDeneme[4]);
+
+//Date - Tarih Değerleri
+// var date = new Date();
+// console.log(date);
+// console.log(typeof date);
+// console.log(date.getFullYear());
+
+a = 10;
+b = a;
+console.log("1- ", a, b);
---
+++ Ogrenciler/Ahmet_Kanbaz/Dersler/Ders20-Array-Object/script.js
@@ -0,0 +1,52 @@
+// //Array Özellikleri
+
+// let numbers = [200, 14, 14,  37, 41, 57, 63, 74, 81, 99, 104];
+
+// value = numbers.sort(function(x, y)
+// {return x-y});
+// console.log(value);
+
+// const user = {
+//   name: "Ahmet KANBAZ",
+//   age: 24,
+//   email: "ahmetkanbaz@gmail.com",
+//   langs: ["Python", "JavaScript", "C++"],
+//   adress: {
+//     city: "Mersin",
+//     street: "Akdeniz",
+//   },
+//   work: function () {
+//     console.log("Çalışmaktadır.")
+//   }
+// }
+
+// value = user.adress.city;
+// value = user.work();
+
+// console.log(value);
+
+// let randDeger = Math.floor(Math.random() * 100 + 1);
+// console.log(randDeger);
+// let sayac = 0;
+
+// let randomNumber = Math.floor(Math.random() * 100) + 1;
+// console.log(randomNumber)
+// let counter =0;
+// let tahmin;
+// let score = [100,75,50,25]
+// for(let i=0; randomNumber != tahmin ;i++){
+//     counter++;
+//     tahmin = prompt("Sayı tahmin ediniz!")
+//     if(tahmin>randomNumber){
+//         console.log("Aşağı")
+//     }
+//     else if(tahmin<randomNumber){
+//         console.log("yukarı")
+//     }
+// }
+// console.log(`Tebrikler ${counter} defada bildiniz :D`)
+// counter < 2 ? console.log("Skorunuz",score[0]): 
+// counter < 5? console.log("Skorunuz :",score[1]):
+// counter < 7? console.log("Skorunuz : ",score[2]):
+// console.log("Skorunuz : ",score[3])
+
