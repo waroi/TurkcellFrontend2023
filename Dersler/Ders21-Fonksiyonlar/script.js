ed7700a1a5a86f71cba859844d4b0c5760f40ec8
Varol Maksutoglu <varolmaksutoglu@yandex.com>
2023-04-24 19:18:20 +0300

    fonksiyonlar

---
+++ Dersler/Ders21-Fonksiyonlar/script.js
@@ -0,0 +1,40 @@
+// Functions
+// merhaba();
+// function merhaba() {
+//   console.log("Merhaba");
+// }
+
+// function user(name, age) {
+//   console.log(`İsim: ${name} Yaş: ${age}`);
+// }
+
+// user("Varol", 37);
+// user("Maksutoğlu", 38);
+
+// function user(name = "Bilgi Yok", age = "Bilgi Yok") {
+//   console.log(`İsim: ${name} Yaş: ${age}`);
+// }
+
+// user("Varol");
+// user();
+
+// Return ile dışarı değer döndürme
+// function square(x) {
+//   return x * x;
+// }
+// const sonuc = square(5) * 3;
+// console.log(sonuc);
+
+// function cube(x) {
+//   return x * x * x;
+// }
+// const sonuc = cube(5) + 10;
+// console.log(sonuc);
+
+// /**
+//  * Rastgele Başlık xD
+//  * @param {string} x
+//  */
+// function test(x) {
+// }
+// test();
