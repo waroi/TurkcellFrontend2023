cc88a9800742ac2d7b1de32d518b18a1b0adc85b
Burcu Yaman <bburcu0003@gmail.com>
2023-04-25 23:19:44 +0300

    calculator

--- Ogrenciler/BurcuYaman/GrupÖdevleri/Ders21-HesapMakinesi/script.js
+++ Ogrenciler/BurcuYaman/GrupÖdevleri/Ders21-HesapMakinesi/script.js
@@ -20,30 +20,39 @@ function bolme(sayi1, sayi2) {
 }
 
 
-let sayi1 = Number(prompt("İlk sayı giriniz:"));
-let sayi2 = Number(prompt("İkinci sayı giriniz:"));
+let rule = true;
+do {
+    let answer = prompt("devam etmek ister misin?");
 
-let operation = prompt("Bu sayı ile ne yapmak istediğinizi seçiniz?\n1:Toplama\n2:Çıkarma\n3:Çarpma\n4:Bölme")
+    if (answer == "evet") {
+        console.log("devam");
+        let operation = prompt("Bu sayı ile ne yapmak istediğinizi seçiniz?\n1:Toplama\n2:Çıkarma\n3:Çarpma\n4:Bölme")
 
-if (isNaN(sayi1) || isNaN(sayi2)) {
-    alert("Hata: Lütfen geçerli bir sayı girin.");
-} else {
-    switch (operation) {
-        case "1":
-            console.log(toplama(sayi1, sayi2));
-            break;
-        case "2":
-            console.log(cikarma(sayi1, sayi2));
-            break;
-        case "3":
-            console.log(carpma(sayi1, sayi2));
-            break;
-        case "4":
-            console.log(bolme(sayi1, sayi2));
-            break;
+        let sayi1 = Number(prompt("İlk sayı giriniz:"));
+        let sayi2 = Number(prompt("İkinci sayı giriniz:"));
+
+        if (isNaN(sayi1) || isNaN(sayi2)) {
+            alert("Hata: Lütfen geçerli bir sayı girin.");
+        } else {
+            switch (operation) {
+                case "1":
+                    console.log(toplama(sayi1, sayi2));
+                    break;
+                case "2":
+                    console.log(cikarma(sayi1, sayi2));
+                    break;
+                case "3":
+                    console.log(carpma(sayi1, sayi2));
+                    break;
+                case "4":
+                    console.log(bolme(sayi1, sayi2));
+                    break;
+            }
+        }
     }
-}
-
-
-
+    else {
+        console.log("dur");
+        rule = false;
+    }
+} while (rule)
 
