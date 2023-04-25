23e3b6e1dcef59d89e4603265c8b4e8342e5c125
smartbreath <ahmetkanbaz@gmail.com>
2023-04-25 13:34:32 +0300

    Ders21-Fonksiyonlar-Odev
    Arrow function kullanıldı. Girilen değerlerin number olup olmadığı ve seçilen işlemin doğru olup olmadığı kontrol edildi.

--- Ogrenciler/Ahmet_Kanbaz/Dersler/Ders21-Fonksiyonlar/Odev/script.js
+++ Ogrenciler/Ahmet_Kanbaz/Dersler/Ders21-Fonksiyonlar/Odev/script.js
@@ -1,60 +1,107 @@
 let decision = true;
+let firstNumberName = "Birinci";
+let secondNumberName = "İkinci";
+
+const toplama = (number1, number2) => number1 + number2;
+
+const cikarma = (number1, number2) => number1 - number2;
+
+const carpma = (num1, num2) => num1 * num2;
+
+const bolme = (num1, num2) => num1 / num2;
+
+const modAlma = (num1, num2) => num1 % num2;
 
 while (decision) {
   let firstNumber = Number(prompt("Birinci Sayıyı Giriniz:"));
-  while (isNaN(firstNumber)) {
-    firstNumber = Number(
-      prompt("Lütfen Doğru Değer Giriniz.\nBirinci Sayıyı Giriniz:")
-    );
-  }
+  firstNumber = isNotNumber(firstNumber, firstNumberName);
+
   let secondNumber = Number(prompt("İkinci Sayıyı Giriniz:"));
-  while (isNaN(secondNumber)) {
-    secondNumber = Number(
-      prompt("Lütfen Doğru Değer Giriniz.\nİkinci Sayıyı Giriniz:")
-    );
-  }
+  secondNumber = isNotNumber(secondNumber, secondNumberName);
   let operation = Number(prompt(
     "Yapmak istediğiniz işlemin numarasını giriniz\n1- Toplama\n2- Çıkarma\n3- Çarpma\n4- Bölme\n5- Üs Alma\n6- Mod ALma"
   ));
+  operation = isNotNumberOperation(operation);
 
   switch (operation) {
     case 1:
       let sumResult = toplama(firstNumber, secondNumber);
-      alert(`${firstNumber} ve ${secondNumber} sayılarının toplamı: ${sumResult}`);
+      alert(
+        `${firstNumber} ve ${secondNumber} sayılarının toplamı: ${sumResult}`
+      );
       break;
     case 2:
       let subtractionResult = cikarma(firstNumber, secondNumber);
-      alert(`${firstNumber} sayısından ${secondNumber} sayısını çıkardığımızda sonuç: ${subtractionResult}`);
+      alert(
+        `${firstNumber} sayısından ${secondNumber} sayısını çıkardığımızda sonuç: ${subtractionResult}`
+      );
       break;
     case 3:
       let carpmaResult = carpma(firstNumber, secondNumber);
-      alert (`${firstNumber} ve ${secondNumber} sayılarının çarpımı sonucu: ${carpmaResult}`);
+      alert(
+        `${firstNumber} ve ${secondNumber} sayılarının çarpımı sonucu: ${carpmaResult}`
+      );
       break;
-    
+    case 4:
+      let bolmeSonuc = bolme(firstNumber, secondNumber);
+      alert(
+        `${firstNumber} sayısının ${secondNumber} sayısına bölümünün sonucu: ${bolmeSonuc}`
+      );
+      break;
+    case 5:
+      let usSonuc = usAlma(firstNumber, secondNumber);
+      alert(`${firstNumber} sayısının ${secondNumber} üssü sonucu: ${usSonuc}`);
+      break;
+    case 6:
+      let modSonuc = modAlma(firstNumber, secondNumber);
+      alert(
+        `${firstNumber} sayısının ${secondNumber} sayısına bölümünden kalan: ${modSonuc}`
+      );
+      break;
+
     default:
-      alert("Lütfen Doğru Bir Seçim Yapınız...");
+      operation = isNotNumberOperation(operation);
   }
 
-  // decisionFunction();
+  decisionFunction();
 }
 
-function toplama(number1, number2) {
-  return number1 + number2;
+function isNotNumber(number, name) {
+  while (isNaN(number)) {
+    number = Number(
+      prompt(`Lütfen Doğru Değer Giriniz.\n${name} Sayıyı Giriniz:`)
+    );
+  }
+  return number;
 }
 
-function cikarma(number1, number2) {
-  return number1 - number2;
+function isNotNumberOperation(operation) {
+  while(isNaN(operation) || !(0 < operation && operation < 7)) {
+    operation = Number(prompt(
+      "Lütfen yapmak işlemin numarasını doğru giriniz!!!\n1- Toplama\n2- Çıkarma\n3- Çarpma\n4- Bölme\n5- Üs Alma\n6- Mod ALma"
+    ));
+  }
+  return operation;
 }
 
-function carpma(num1, num2) {
-  return num1 * num2;
+function usAlma(num1, num2) {
+  let result = 1;
+  if (num2 < 0) {
+    for (let i = 0; num2 < i; i--) {
+      result /= num1;
+    }
+  } else {
+    for (let i = 0; i < num2; i++) {
+      result *= num1;
+    }
+  }
+  return result;
 }
 
 function decisionFunction() {
   while (true) {
     let valueDecision = prompt("Devam etmek istiyor musunuz? (E/H)");
     if (valueDecision == "E" || valueDecision == "e") {
-      decision = true;
       break;
     } else if (valueDecision == "H" || valueDecision == "h") {
       decision = false;
