e3c84655a9d1fa50929c60ae645470e50ce0e236
İrem Gündoğan <rem1035@hotmail.com>
2023-04-27 18:06:12 +0300

    update

---
+++ Ogrenciler/IremGundogan/BireyselCalismalarim/JS/part-1/script.js
@@ -0,0 +1,14 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Irem");
+console.log(22);
+
+let firstName = "Mahmut";
+// let first = "Irem";
+// let firstNamePerson;
+console.log(firstName);
+
+// let 3years=3; yanlış
+let irem_Mahmut = "IM";
+let $function = 27;
