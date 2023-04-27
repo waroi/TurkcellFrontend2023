3a74bab7e1dd7c35acdc5385683b1ab3fed45c86
Seyit Yuce <111066041+seyityuce@users.noreply.github.com>
2023-04-27 16:00:02 +0300

    todoapp-filter
    Co-Authored-By: Ege <83390653+Egebyte@users.noreply.github.com>
    Co-Authored-By: Selahattin Demir <87477482+Selahaddin64@users.noreply.github.com>

--- Ogrenciler/SelahattinDemir/Ders_Sonu_Odevleri/js/teamworkTodo/app.js
+++ Ogrenciler/SelahattinDemir/Ders_Sonu_Odevleri/js/teamworkTodo/app.js
@@ -1,25 +1,28 @@
 const addToDoBtn = document.querySelector("#btn-add");
 const clearToDos = document.querySelector("#btn-clr");
-
 const addToDoInp = document.getElementById("inp-enter");
 const searchToDo = document.getElementById("inp-search");
-
 const toDoList = document.getElementById("items");
-
 const toDos = [];
-
 addToDoBtn.addEventListener("click", addToDo);
 clearToDos.addEventListener("click", clearToDo);
 
 // ToDo Ekleme
-function addToDo() {
+function addToDo(e) {
+  e.preventDefault();
   const toDo = addToDoInp.value;
-  toDos.push(toDo);
-  addToDoInp.value = "";
-  showToDo();
+  const toDoCheck = toDo.replace(/ /g, "");
+
+  if (toDoCheck === "") {
+    alert("Lütfen boş");
+  } else {
+    toDos.push(toDo);
+    addToDoInp.value = "";
+    showToDo(toDos);
+  }
 }
 // ToDo Gösterme
-function showToDo() {
+function showToDo(toDos) {
   const listItem = document.createElement("li");
   const xCircle = document.createElement("i");
   const toDoText = document.createTextNode(toDos[toDos.length - 1]);
@@ -28,11 +31,24 @@ function showToDo() {
   listItem.appendChild(toDoText);
   listItem.appendChild(xCircle);
   toDoList.appendChild(listItem);
-  listItem.addEventListener("click", () => {
+  xCircle.addEventListener("click", () => {
     deleteToDo(listItem);
   });
 }
 // ToDo Arama
+searchToDo.addEventListener("keyup", searchToDos);
+function searchToDos() {
+  const toDoSearch = searchToDo.value.toLowerCase();
+  console.log(toDoSearch);
+  Array.from(toDoList.children).forEach((y) => {
+    console.log(y);
+    if (!y.textContent.includes(toDoSearch)) {
+      y.className = "d-none";
+    } else {
+      y.className = "d-flex justify-content-between align-items-center";
+    }
+  });
+}
 
 // ToDo Silme
 function deleteToDo(listItem) {
