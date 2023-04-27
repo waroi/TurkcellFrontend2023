b19fce65c0f7725865297af8e1605eb2d5c87cdc
Mücahit Kuk <mucahit.kuk@gmail.com>
2023-04-27 19:08:44 +0300

    calculator

--- Ogrenciler/Mucahitkuk/Grup/grup-11-mucahit-aytug-selim/script.js
+++ Ogrenciler/Mucahitkuk/Grup/grup-11-mucahit-aytug-selim/script.js
@@ -1,88 +1,62 @@
+const todoInput = document.querySelector("#todo-input");
+const addBtn = document.querySelector("#add-todo");
+const todoList = document.querySelector("#todo-list");
+const removeBtn = document.querySelector("#delete-all");
+const errorText = document.querySelector("#error-text");
+const searchTodo = document.querySelector("#search-todo");
+addBtn.addEventListener("click", addTodo);
+removeBtn.addEventListener("click", clearTodoList);
+searchTodo.addEventListener("input", searchTodos);
 
-const todoInput = document.querySelector('#todo-input');
-const addBtn = document.querySelector('#add-todo');
-const todoList = document.querySelector('#todo-list');
-const removeBtn = document.querySelector('#delete-todo');
-addBtn.addEventListener('click', addTodo);
-removeBtn.addEventListener('click', clearTodoList);
-
-
-let list = document.getElementById('todo-list');
+let list = document.getElementById("todo-list");
 
 function addTodo() {
-  const newTodo = document.createElement('li');
+  if (todoInput.value == "") {
+    return (errorText.innerText = "Lütfen Bir Todo Giriniz.");
+  }
+  errorText.innerText = "";
+  const newTodo = document.createElement("li");
+  const newButton = document.createElement("button");
+  newTodo.classList.add(
+    "d-flex",
+    "justify-content-between",
+    "align-items-center",
+    "relative",
+    "l-0",
+    "bg-danger",
+    "p-2",
+    "rounded",
+    "outline-0",
+    "mb-2",
+    "text-white"
+  );
+  newButton.classList.add("bg-transparent", "me-3", "border-0", "delete-todo");
   newTodo.innerText = todoInput.value;
+  newButton.innerText = "X";
+  newTodo.appendChild(newButton);
   list.appendChild(newTodo);
+  const deleteTodos = document.querySelectorAll(".delete-todo");
+  deleteTodos.forEach((deleteTodo) => {
+    deleteTodo.addEventListener("click", clearTodo);
+  });
+  todoInput.value = "";
+}
+
+function searchTodos(searchTodo) {
+  example = document.querySelectorAll("li");
+  example.forEach((i) => {
+    if (i.innerText.includes(searchTodo.target.value)) {
+      i.classList.remove("d-none");
+    } else {
+      i.classList.add("d-none");
+    }
+  });
 }
 
 function clearTodoList() {
-  list.remove('todo-list');
+  list.innerHTML = "";
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Selectors
-// const todoInput = document.querySelector('#todo-input');
-// const addBtn = document.querySelector('#add-btn');
-// const todoList = document.querySelector('#todo-list');
-
-// // Event listeners
-// addBtn.addEventListener('click', addTodo);
-// todoList.addEventListener('click', deleteTodo);
-
-// // Functions
-// function addTodo() {
-//   // Create new list item
-//   const newTodo = document.createElement('li');
-//   newTodo.innerText = todoInput.value;
-  
-//   // Add delete button
-//   const deleteBtn = document.createElement('button');
-//   deleteBtn.innerText = 'Delete';
-//   newTodo.appendChild(deleteBtn);
-  
-//   // Add new todo to list
-//   todoList.appendChild(newTodo);
-  
-//   // Clear input field
-//   todoInput.value = '';
-// }
-
-// function deleteTodo(e) {
-//   if (e.target.tagName === 'BUTTON') {
-//     e.target.parentElement.remove();
-//   }
-// }
+function clearTodo() {
+  return document.querySelector("li").remove();
+}
