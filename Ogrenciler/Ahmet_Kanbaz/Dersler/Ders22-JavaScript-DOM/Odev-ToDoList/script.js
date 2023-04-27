c744fea12b6885fd392185cec139a368818ce9aa
smartbreath <ahmetkanbaz@gmail.com>
2023-04-27 20:35:47 +0300

    Ders22-JavaScript-DOM-ToDoList

---
+++ Ogrenciler/Ahmet_Kanbaz/Dersler/Ders22-JavaScript-DOM/Odev-ToDoList/script.js
@@ -0,0 +1,33 @@
+const addTaskForm = document.getElementById('addTask');
+const addTaskInput = document.getElementById('taskName');
+
+const taskList = document.getElementById('taskList');
+const deleteAllTask = document.getElementById('deleteAll');
+
+addEventListener();
+
+function addEventListener() {
+  addTaskForm.addEventListener('submit', addNewToDoTask);
+}
+
+function addNewToDoTask(e) {
+  if(addTaskInput.value === '') {
+    alert('Lütfen New Task İnput Kısmını Doldurunuz!!!');
+  }
+
+  const newItemLi = document.createElement('li');
+  newItemLi    .className = 'list-group-item list-group-item-secondary';
+  const itemContext = document.createTextNode(addTaskInput.value);
+  newItemLi    .appendChild(itemContext);
+  const itemDeleteIcon = document.createElement('a');
+  itemDeleteIcon.className = 'deleteButton float-end';
+  itemDeleteIcon.href = '#';
+  const itemDeleteSpan = document.createElement('span');
+  itemDeleteSpan.className = 'fa-solid fa-xmark';
+  itemDeleteIcon.appendChild(itemDeleteSpan);
+  newItemLi.appendChild(itemDeleteIcon);
+  taskList.appendChild(newItemLi    );
+  addTaskInput.value = '';
+  
+  e.preventDefault();
+}
