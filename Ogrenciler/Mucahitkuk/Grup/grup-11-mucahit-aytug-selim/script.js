1684431c5de82da9e3b5cb1d9cb1c06b5d37b824
Mücahit Kuk <mucahit.kuk@gmail.com>
2023-04-28 23:43:37 +0300

    refactor and features added, bug fixes.

--- Ogrenciler/Mucahitkuk/Grup/grup-11-mucahit-aytug-selim/script.js
+++ Ogrenciler/Mucahitkuk/Grup/grup-11-mucahit-aytug-selim/script.js
@@ -6,112 +6,146 @@ const errorText = document.querySelector("#error-text");
 const searchTodo = document.querySelector("#search-todo");
 
 addBtn.addEventListener("click", inputCheck);
-removeBtn.addEventListener("click", clearTodoList);
+removeBtn.addEventListener("click", removeLocal);
 searchTodo.addEventListener("input", searchTodos);
 
 let list = document.getElementById("todo-list");
-let toDoLocalList = [];
 
-if (localStorage.getItem("todolist")) {
-	toDoLocalList = JSON.parse(localStorage.getItem("todolist"));
-} else {
-	localStorage.setItem("todolist", JSON.stringify(toDoLocalList));
+function inputCheck(e) {
+  e.preventDefault();
+  if (todoInput.value.trim() === "") {
+    errorText.textContent = "Please enter a task.";
+  } else if (todoInput.value.length > 50) {
+    (errorText.textContent = "Lütfen 50 Karakteri Aşmayınız");
+  }
+  else {
+    errorText.textContent = "";
+    addTodo();
+  }
 }
 
-function removeLocal() {
-	toDoLocalList = [];
-	localStorage.setItem("todolist", JSON.stringify(toDoLocalList));
+function addTodo() {
+  const todo = {
+    id: Date.now(),
+    text: todoInput.value.trim(),
+    completed: false
+  };
+  saveToLocalStorage(todo);
+  renderTodo(todo);
+  todoInput.value = "";
 }
 
-function inputCheck() {
-	if (todoInput.value.trim("") == "") {
-		return (errorText.innerText = "Lütfen Bir Todo Giriniz.");
-	} else if (todoInput.value.length > 50) {
-		return (errorText.innerText = "Lütfen 50 Karakteri Aşmayınız");
-	}
-	errorText.innerText = "";
-
-	addTodo(todoInput.value);
+function renderTodo(todo) {
+  const newTodo = document.createElement("li");
+    const newButton = document.createElement("button");
+    newTodo.classList.add(
+        "d-flex",
+        "justify-content-between",
+        "align-items-center",
+        "relative",
+        "l-0",
+        "bg-danger",
+        "p-2",
+        "rounded",
+        "outline-0",
+        "mb-2",
+        "text-white"
+    );
+    newButton.classList.add("bg-transparent", "me-3", "border-0", "delete-todo");
+    newTodo.innerText = todo.text;
+    newButton.classList.add("bi", "bi-x-square-fill", "text-white");
+    list.appendChild(newTodo);
+    newTodo.appendChild(newButton);
+		newTodo.querySelector(".delete-todo").addEventListener("click", deleteTodo);
+    todoInput.value = "";
 }
 
-function addTodo(input) {
-	const newTodo = document.createElement("li");
-	const newButton = document.createElement("button");
-	newTodo.classList.add(
-		"d-flex",
-		"justify-content-between",
-		"align-items-center",
-		"relative",
-		"l-0",
-		"bg-danger",
-		"p-2",
-		"rounded",
-		"outline-0",
-		"mb-2",
-		"text-white"
-	);
-	newButton.classList.add("bg-transparent", "me-3", "border-0", "delete-todo");
-	newTodo.innerText = input;
-	toDoLocalList.push(newTodo.innerText);
-	localStorage.setItem("todolist", JSON.stringify(toDoLocalList));
-	newButton.classList.add("bi", "bi-x-square-fill", "text-white");
-	list.appendChild(newTodo);
-	newTodo.appendChild(newButton);
-	const deleteTodos = document.querySelectorAll(".delete-todo");
-	deleteTodos.forEach((deleteTodo) => {
-		deleteTodo.addEventListener("click", clearTodo);
-	});
-	todoInput.value = "";
+
+function searchTodos(e) {
+  const searchTerm = e.target.value.toLowerCase();
+  const todos = list.getElementsByTagName("li");
+  Array.from(todos).forEach(todo => {
+    const todoText = todo.querySelector("span").textContent.toLowerCase();
+    if (todoText.includes(searchTerm)) {
+      todo.style.display = "block";
+    } else {
+      todo.style.display = "none";
+    }
+  });
 }
 
-function searchTodos(searchTodo) {
-	example = document.querySelectorAll("li");
-	example.forEach((i) => {
-		if (i.innerText.includes(searchTodo.target.value)) {
-			i.classList.remove("d-none");
-		} else {
-			i.classList.add("d-none");
-		}
-	});
+function searchTodos(e) {
+  const searchTerm = e.target.value.toLowerCase();
+  const todos = list.getElementsByTagName("li");
+  Array.from(todos).forEach(todo => {
+    const todoText = todo.innerText;
+    if (todoText.includes(searchTerm)) {
+      todo.style.display = "block";
+    } else {
+      todo.style.cssText = "display:none !important";
+    }
+  });
 }
 
-function clearTodoList() {
-	list.innerHTML = "";
-	removeLocal();
-	ListTodos();
+
+function saveToLocalStorage(todo) {
+  let todos = getTodosFromLocalStorage();
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-let newLocal = [];
-
-function clearTodo() {
-	newLocal = JSON.parse(localStorage.getItem("todolist"));
-	let den = document.querySelector("li").innerText;
-	toDoLocalList = newLocal.filter((number) => number != den);
-
-	localStorage.setItem("todolist", JSON.stringify(toDoLocalList));
-	ListTodos();
-	return document.querySelector("li").remove();
+function getTodosFromLocalStorage() {
+  let todos = localStorage.getItem("todos");
+  return todos ? JSON.parse(todos) : [];
 }
 
-function ListTodos() {
-	toDoLocalList = JSON.parse(localStorage.getItem("todolist"));
-	toDoLocalList.map((item) => {
-		console.log(item);
-		addTodo(item);
-	});
+function toggleCompleted(e) {
+  const li = e.target.parentElement;
+  const id = li.getAttribute("data-id");
+  let todos = getTodosFromLocalStorage();
+
+  todos = todos.map(todo => {
+    if (todo.id.toString() === id) {
+      return { ...todo, completed: !todo.completed };
+    }
+    return todo;
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
+
+function deleteTodo(e) {
+  const li = e.target.closest("li");
+  const text = li.innerText;
+
+  let todos = getTodosFromLocalStorage();
+  todos = todos.filter(todo => todo.text !== text);
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+  li.remove();
+}
+
+
+ function removeLocal() {
+  list.innerHTML = "";
+ 	todos = [];
+ 	localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function loadTodosFromLocalStorage() {
+  const todos = getTodosFromLocalStorage();
+  todos.forEach(todo => renderTodo(todo));
+}
+
+loadTodosFromLocalStorage();
 
 const clockTodo = document.querySelector("#date");
 
 function clock() {
-	let date = new Date().toString().slice(16, 25);
-	clockTodo.innerText = date;
+    let date = new Date().toString().slice(16, 25);
+    clockTodo.innerText = date;
 }
-
-ListTodos();
 
 clockTodo.addEventListener("change", clock);
 
 setInterval(clock, 1000);
-
-// SAYFA YENİLENDİĞİNDE LOCAL STOGRAGEYE AYNI VERİLEREİ EKLEME SORUNU
