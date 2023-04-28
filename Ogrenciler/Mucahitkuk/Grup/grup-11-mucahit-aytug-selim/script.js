8a4324ba6071e756c7a41f422cf6b22dadb1de77
Mücahit Kuk <mucahit.kuk@gmail.com>
2023-04-28 18:32:40 +0300

    todolist

--- Ogrenciler/Mucahitkuk/Grup/grup-11-mucahit-aytug-selim/script.js
+++ Ogrenciler/Mucahitkuk/Grup/grup-11-mucahit-aytug-selim/script.js
@@ -4,6 +4,7 @@ const todoList = document.querySelector("#todo-list");
 const removeBtn = document.querySelector("#delete-all");
 const errorText = document.querySelector("#error-text");
 const searchTodo = document.querySelector("#search-todo");
+
 addBtn.addEventListener("click", addTodo);
 removeBtn.addEventListener("click", clearTodoList);
 searchTodo.addEventListener("input", searchTodos);
@@ -11,8 +12,10 @@ searchTodo.addEventListener("input", searchTodos);
 let list = document.getElementById("todo-list");
 
 function addTodo() {
-  if (todoInput.value == "") {
+  if (todoInput.value.trim("") == "") {
     return (errorText.innerText = "Lütfen Bir Todo Giriniz.");
+  } else if (todoInput.value.length > 50) {
+    return (errorText.innerText = "Lütfen 50 Karakteri Aşmayınız");
   }
   errorText.innerText = "";
   const newTodo = document.createElement("li");
@@ -32,9 +35,9 @@ function addTodo() {
   );
   newButton.classList.add("bg-transparent", "me-3", "border-0", "delete-todo");
   newTodo.innerText = todoInput.value;
-  newButton.innerText = "X";
-  newTodo.appendChild(newButton);
+  newButton.classList.add("bi", "bi-x-square-fill", "text-white");
   list.appendChild(newTodo);
+  newTodo.appendChild(newButton);
   const deleteTodos = document.querySelectorAll(".delete-todo");
   deleteTodos.forEach((deleteTodo) => {
     deleteTodo.addEventListener("click", clearTodo);
@@ -60,3 +63,15 @@ function clearTodoList() {
 function clearTodo() {
   return document.querySelector("li").remove();
 }
+
+
+const clockTodo = document.querySelector("#date");
+
+function clock() {
+  let date = new Date().toString().slice(16, 25);
+  clockTodo.innerText = date;
+}
+
+clockTodo.addEventListener("change", clock);
+
+setInterval(clock, 1000);
