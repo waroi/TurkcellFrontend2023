d2d52cbf800733395355368b9a24378d53fa7140
Sueda Çiçekli <suedacicekli@gmail.com>
2023-04-28 20:20:59 +0300

    todo

--- Ogrenciler/SuedaCicekli/TeamWork/TeamWork12-Sueda-Guzin-Kagan-Osman/Todo/script.js
+++ Ogrenciler/SuedaCicekli/TeamWork/TeamWork12-Sueda-Guzin-Kagan-Osman/Todo/script.js
@@ -4,36 +4,52 @@ let search = document.getElementById("search");
 
 eventListener();
 
+window.addEventListener("DOMContentLoaded", render);
+
 function eventListener() {
   form.addEventListener("submit", addTodo);
-  deletedItem.addEventListener("click", deleteAllItem);
+  deletedItem.addEventListener("click", () => {
+    deleteAllItem();
+    delStorage();
+  });
   search.addEventListener("keyup", searchTodo);
 }
 
 function addTodo(e) {
   let todo
   let input = document.getElementById("entryTodo").value;
+  document.getElementById("entryTodo").value = '';
   e.preventDefault();
   getStorage() != null ? todo = getStorage() : todo = [];
   todo.push(input);
   addStorage(todo);
   console.log(getStorage());
-  addItem(input);
+  render();
 }
 
-function addItem(input) {
-  const item = document.createElement("li");
-  item.className = "todoItem d-flex justify-content-between my-1";
-  const deleteButton = document.createElement("button");
-  let deleteIcon = document.createElement("i");
-  deleteIcon.setAttribute("class", "fa-solid fa-trash");
-  deleteButton.appendChild(deleteIcon);
-  deleteButton.className = "deleteItemButton btn btn-danger";
-  deleteButton.addEventListener("click", function () { item.remove(); })
-  const text = document.createTextNode(input);
-  item.appendChild(text);
-  item.appendChild(deleteButton);
-  document.getElementById("todoItems").appendChild(item);
+function render() {
+  deleteAllItem();
+  let todos = getStorage();
+  todos.map((todo, index) => {
+    const item = document.createElement("li");
+    item.className = "todoItem d-flex justify-content-between my-1";
+    const deleteButton = document.createElement("button");
+    let deleteIcon = document.createElement("i");
+    deleteIcon.setAttribute("class", "fa-solid fa-trash");
+    deleteButton.appendChild(deleteIcon);
+    deleteButton.className = "deleteItemButton btn btn-danger";
+    deleteButton.addEventListener("click", function () {
+      item.remove();
+      console.log(index, todos[index]);
+      todos.splice(index, 1);
+      addStorage(todos);
+      render();
+    })
+    const text = document.createTextNode(todo);
+    item.appendChild(text);
+    item.appendChild(deleteButton);
+    document.getElementById("todoItems").appendChild(item);
+  })
 }
 
 function deleteAllItem() {
@@ -51,7 +67,6 @@ function searchTodo(e) {
 
   todoItems.forEach(function (listItem) {
     const text = listItem.textContent.toLowerCase();
-    console.log(text);
     if (text.indexOf(filterValue) === -1) {
       listItem.setAttribute("style", "display : none !important");
     } else {
@@ -68,5 +83,9 @@ function addStorage(storageArray) {
 function getStorage() {
   return JSON.parse(localStorage.getItem("data"));
 
+}
+
+function delStorage() {
+  localStorage.clear();
 }
 
