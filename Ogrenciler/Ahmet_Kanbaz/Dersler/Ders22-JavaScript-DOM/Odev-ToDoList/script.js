95c80a457f26025cb8cbe096de7ca7f82c2bce84
smartbreath <ahmetkanbaz@gmail.com>
2023-05-01 21:02:30 +0300

    Ders22-JavaScript-ToDoList

--- Ogrenciler/Ahmet_Kanbaz/Dersler/Ders22-JavaScript-DOM/Odev-ToDoList/script.js
+++ Ogrenciler/Ahmet_Kanbaz/Dersler/Ders22-JavaScript-DOM/Odev-ToDoList/script.js
@@ -15,19 +15,24 @@ function addNewToDoTask(e) {
     alert('Lütfen New Task İnput Kısmını Doldurunuz!!!');
   }
 
-  const newItemLi = document.createElement('li');
-  newItemLi    .className = 'list-group-item list-group-item-secondary';
-  const itemContext = document.createTextNode(addTaskInput.value);
-  newItemLi    .appendChild(itemContext);
-  const itemDeleteIcon = document.createElement('a');
-  itemDeleteIcon.className = 'deleteButton float-end';
-  itemDeleteIcon.href = '#';
-  const itemDeleteSpan = document.createElement('span');
-  itemDeleteSpan.className = 'fa-solid fa-xmark';
-  itemDeleteIcon.appendChild(itemDeleteSpan);
-  newItemLi.appendChild(itemDeleteIcon);
-  taskList.appendChild(newItemLi    );
-  addTaskInput.value = '';
+  else {
+    const newItemLi = document.createElement('li');
+    newItemLi.className = 'list-group-item list-group-item-secondary';
+    const itemContext = document.createTextNode(addTaskInput.value);
+    newItemLi.appendChild(itemContext);
+    const itemDeleteIcon = document.createElement('a');
+    itemDeleteIcon.className = 'deleteButton float-end';
+    itemDeleteIcon.href = '#';
+    const itemDeleteSpan = document.createElement('span');
+    itemDeleteSpan.className = 'fa-solid fa-xmark';
+    itemDeleteIcon.appendChild(itemDeleteSpan);
+    newItemLi.appendChild(itemDeleteIcon);
+    taskList.appendChild(newItemLi);
+    addTaskInput.value = '';
+  }
   
   e.preventDefault();
 }
+
+let a = "I love React".split("").reverse().join("");
+console.log(a);
