4c7149bf7828fc75dbd9d38f2cada6677706bc74
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-05-01 11:41:13 +0300

    completed

--- Ogrenciler/OmerFarukCeltik/homeworks/odev13-todoapp/app.js
+++ Ogrenciler/OmerFarukCeltik/homeworks/odev13-todoapp/app.js
@@ -4,36 +4,16 @@ let search_input = document.querySelector("#search_todo");
 let todo_list = document.querySelector("#todo_list");
 let clear_all = document.querySelector("#clear_all");
 let list_item = document.querySelectorAll("li");
-let local_value = JSON.parse(localStorage.getItem("todos")) || [];
+let local_value =  localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
 let todo_inner={};
 
 add_button.addEventListener("click", addTodo);
-todo_value.addEventListener("keypress",(e) => e.key == "Enter" ? addTodo() : "");
+todo_value.addEventListener("keypress",(e) => e.key == "Enter" ? addTodo(e) : "");
 todo_list.addEventListener("click",removeTodo);
 clear_all.addEventListener("click",clearAllTodos);
+search_input.addEventListener("keyup", filterTodos)
 
-
-function listTodos() {
- console.log(local_value);
- local_value.forEach(element => {
-  console.log(element);
-  createTodo(element);
- });
-}
-listTodos();
-function createTodo(todo) {
-  let listItem = document.createElement("li");
-  listItem.classList = `${todo.isDone == true ? "bg-success" : ""} list-group-item d-flex justify-content-between align-items-center`;
-  listItem.innerText = todo.value;
-  listItem.id = todo.id;
-  let tag = document.createElement("span");
-  tag.classList = "fa-solid fa-x fs-6 text-secondary"
-  listItem.appendChild(tag);
-  todo_list.appendChild(listItem);
-  console.log(listItem.id);
-}
-
-function addTodo(){
+function addTodo(e){
   let value = todo_value.value.trim();
   todo_inner = {
     id: Math.round(Math.random() *10000),
@@ -41,12 +21,47 @@ function addTodo(){
     isDone: false,
   }
   if(value !== ""){
-    local_value = [...local_value, todo_inner];
+    e.preventDefault();
+    local_value.push(todo_inner);
     localStorage.setItem("todos",JSON.stringify(local_value));
     todo_value.value = "";
-    listTodos();
+    createTodo(todo_inner);
   }else{
     alert("please enter a todo");
+  }
+}
+
+ local_value.forEach(element => {
+   createTodo(element);
+  }
+ );
+
+function createTodo(todo) {
+  let listItem = document.createElement("li");
+  listItem.classList = `${todo.isDone == true ? "bg-success" : ""} list-group-item d-flex justify-content-between align-items-center`;
+  listItem.innerText = todo.value;
+  listItem.id = todo.id;
+  let tag = document.createElement("span");
+  tag.classList = "fa-solid fa-x fs-6 text-secondary"
+  todo_list.appendChild(listItem);
+  listItem.appendChild(tag);
+
+}
+function filterTodos(e){
+  let search = e.target.value;
+  for (let i = 0; i < todo_list.children.length; i++) {
+    const text = todo_list.children[i].innerText;
+    let include = text.toLowerCase().indexOf(search.toLowerCase());
+    console.log(include);
+    // console.log(local_value[i]);
+    if (include > -1) {
+      console.log(todo_list.children[i]);
+      todo_list.children[i].classList.add("d-block");
+      todo_list.children[i].classList.remove("d-none");
+
+    }else{
+      todo_list.children[i].classList.add("d-none");
+    }
   }
 }
 function removeTodo(e) {
