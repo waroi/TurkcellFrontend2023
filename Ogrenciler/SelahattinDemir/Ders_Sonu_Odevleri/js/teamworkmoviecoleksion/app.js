5d5f360624b4bd1da9767a7e1c0ef02f5699ce91
Sueda Çiçekli <suedacicekli@gmail.com>
2023-05-04 23:30:20 +0300

    add local storage

--- Ogrenciler/SelahattinDemir/Ders_Sonu_Odevleri/js/teamworkmoviecoleksion/app.js
+++ Ogrenciler/SelahattinDemir/Ders_Sonu_Odevleri/js/teamworkmoviecoleksion/app.js
@@ -1,7 +1,8 @@
 const form = document.querySelector("#film-form");
 const filmList = document.querySelector("#film-list");
 
-function Film(title, director, imageUrl, score, year, kind) {
+function Film(id, title, director, imageUrl, score, year, kind) {
+  this.id = id;
   this.title = title;
   this.director = director;
   this.year = year;
@@ -10,9 +11,25 @@ function Film(title, director, imageUrl, score, year, kind) {
   this.score = score;
 }
 
+let id = checkandgetID();
+
+function checkandgetID() {
+  let id;
+  if (localStorage.getItem("id") === null) {
+    localStorage.setItem("id", 0);
+    id = 0;
+  } else {
+    id = localStorage.getItem("id");
+  }
+  return id;
+}
+
+
+showFilmsFromLocalStorage();
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
+  id = checkandgetID();
   let filmUrl = document.getElementById("filmUrl").value;
   let filmName = document.getElementById("filmName").value;
   let director = document.getElementById("director").value;
@@ -20,68 +37,57 @@ form.addEventListener("submit", (e) => {
   let filmYear = document.getElementById("filmYear").value;
   let filmScore = document.getElementById("filmScore").value;
 
-  let newFilm = new Film(filmName, director, filmUrl, filmScore, filmYear, filmType);
-  filmAdd(newFilm);
-
+  let newFilm = new Film(id, filmName, director, filmUrl, filmScore, filmYear, filmType);
+  saveFilmToLocalStorage(newFilm);
+  showFilmsFromLocalStorage(); //değerleri local storage'dan aldığımız için parametresiz çalıştırdık.
+  localStorage.setItem("id", ++id);
 });
 
 
 
-function filmAdd(film) {
-
-  console.log(film);
-  filmList.innerHTML += `
-  <div class="col-md-4 " id="card">
+function showFilmsFromLocalStorage() {
+  filmList.innerHTML = "";
+  let films = getFilmToLocalStorage();
+  films.forEach((movie) => {
+    filmList.innerHTML += `
+  <div class="col-md-4" id="${movie.id}">
   <div class="card mb-3 mt-5">
         <div class="row align-items-center">
           <div class="col-md-6">
-            <img src=${film.imageUrl}
+            <img src=${movie.imageUrl}
               class="img-fluid rounded-start" id="film-url" alt="" />
           </div>
           <div class="col-md-6">
             <div class="card-body">
-              <h5 class="card-title" id="film-name">${film.title}</h5>
+              <h5 class="card-title" id="film-name">${movie.title}</h5>
               <p class="card-text">
                 <span class="fw-bold" id="
-                director">Yönetmen Adı:</span> ${film.director}
+                director">Yönetmen Adı:</span> ${movie.director}
               </p>
-              <p class="card-text" id="filmType"><span class="fw-bold">Tür Adı:</span>${film.kind}</p>
-              <p class="card-text" id="filmYear"><span class="fw-bold">Yıl:</span>${film.year}</p>
-              <p class="card-text" id="filmScore"><span class="fw-bold">Puan:</span>${film.score}</p>
+              <p class="card-text" id="filmType"><span class="fw-bold">Tür Adı:</span>${movie.kind}</p>
+              <p class="card-text" id="filmYear"><span class="fw-bold">Yıl:</span>${movie.year}</p>
+              <p class="card-text" id="filmScore"><span class="fw-bold">Puan:</span>${movie.score}</p>
             </div>
           </div>
         </div>
         <div class="d-flex justify-content-evenly my-3">
-          <button class="btn btn-danger w-25" id="deleteButton">Sil</button>
-          <button class="btn btn-success w-25" id="editButton">Düzenle</button>
+        <button class="btn btn-danger w-25" onclick="deleteItem(${movie.id})" class="deleteButton">Sil</button>
+        <button class="btn btn-success w-25" id="editButton">Düzenle</button>
         </div>
       </div>
       </div>
 `;
-
-  // Local storage'dan filmleri yükle
-  document.addEventListener("DOMContentLoaded", () => {
-    let films = filmBringLS();
-    films.forEach((film) => {
-      filmAdd(film);
-    });
-  });
-
-  filmSaveLS(film);
-  filmDeleteCard(film);
+  })
 }
 
-function filmDeleteCard(film) {
-  const deleteCard = document.getElementById("card");
-  const deleteButton = document.getElementById("deleteButton");
-  deleteButton.addEventListener("click", () => {
-    filmDeleteLS(film);
-    deleteCard.remove();
-  });
+function deleteItem(id) {
+  let film = document.getElementById(id);
+  deleteFilmToLocalStorage(film);
+  film.remove();
 }
 
 // Local storage'dan filmleri getir
-function filmBringLS() {
+function getFilmToLocalStorage() {
   let films;
   if (localStorage.getItem("films") === null) {
     films = [];
@@ -92,17 +98,17 @@ function filmBringLS() {
 }
 
 // Film objesini local storage'a kaydet
-function filmSaveLS(film) {
-  let films = filmBringLS();
+function saveFilmToLocalStorage(film) {
+  let films = getFilmToLocalStorage();
   films.push(film);
   localStorage.setItem("films", JSON.stringify(films));
 }
 
 // Film objesini local storage'dan sil
-function filmDeleteLS(film) {
-  let films = filmBringLS();
-  films.forEach((f, index) => {
-    if (f.filmName === film.filmName) {
+function deleteFilmToLocalStorage(movie) {
+  let films = getFilmToLocalStorage();
+  films.forEach((film, index) => {
+    if (film.id == movie.id) {
       films.splice(index, 1);
     }
   });
