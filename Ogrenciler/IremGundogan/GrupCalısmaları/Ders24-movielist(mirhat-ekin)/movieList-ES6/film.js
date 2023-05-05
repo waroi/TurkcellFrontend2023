8bf26ea16eb13ff1b65f0603f4defdea245f36db
İrem Gündoğan <rem1035@hotmail.com>
2023-05-05 18:00:00 +0300

    finall

---
+++ Ogrenciler/IremGundogan/GrupCalısmaları/Ders24-movielist(mirhat-ekin)/movieList-ES6/film.js
@@ -0,0 +1,14 @@
+// function Movie(name, director, url, release) {
+//   this.name = name;
+//   this.director = director;
+//   this.url = url;
+//   this.release = release;
+// }
+class Movie {
+  constructor(name, director, url, release) {
+    this.name = name;
+    this.director = director;
+    this.url = url;
+    this.release = release;
+  }
+}
