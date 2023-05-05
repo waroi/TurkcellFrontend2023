610c1552cf6148e5c7f4f7b16cc0f6dbcc02ef47
Aleyna Şenozan <senozanaleyna@gmail.com>
2023-05-05 21:43:55 +0300

    XOX Başlangıç

---
+++ Ogrenciler/aleynaSenozan/Tasks/W9/02-XOX/app.js
@@ -0,0 +1,29 @@
+const game = document.getElementById('game');
+const board = Array(9).fill(null);
+let currentPlayer = 'X';
+
+
+for (let i = 0; i < 9; i++) {
+    const kutu = document.createElement('div');
+    kutu.className = 'kutu';
+    game.appendChild(kutu);
+}
+  
+
+game.addEventListener('click', (e) => {
+  const kutu = e.target;
+  const index = Array.from(game.children).indexOf(kutu);
+
+  if (board[index] === null) {
+    board[index] = currentPlayer;
+    kutu.textContent = currentPlayer;
+
+    if (checkWin()) {
+      alert(`${currentPlayer} Kazandı!`);
+      location.reload();
+    } else {
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+  }
+});
+
