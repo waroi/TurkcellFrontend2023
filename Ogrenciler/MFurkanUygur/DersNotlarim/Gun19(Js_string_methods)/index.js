dced7b5e1e15126cd8bbc00b04de843db1ff9aa9
mfurkanuygur <u.fe.uygur@gmail.com>
2023-05-06 03:19:43 +0200

    something happened

--- Ogrenciler/MFurkanUygur/DersNotlarim/Gun19(Js_string_methods)/index.js
+++ Ogrenciler/MFurkanUygur/DersNotlarim/Gun19(Js_string_methods)/index.js
@@ -1,36 +1,49 @@
 // String Methodları
 
 let value;
-const firstName = "Varol";
-const lastName = "Maksutoğlu";
-const age = 37;
+const firstName = "Furkan uygur";
+const lastName = "UYGUR";
+const age = "37";
 const department = "Bilişim";
 
-// value = firstName + " " + lastName;
-// value = firstName;
-// value += " " + lastName;
+value = firstName + " " + lastName;
+value = firstName;
+value += " " + lastName;
 
-// value = firstName.length;
-// value = firstName.toLowerCase();
-// value = firstName.toUpperCase();
-// value = firstName[0];
-// value = firstName[firstName.length - 1];
-// value = firstName[-1];
-// value = firstName.indexOf("a");
-// value = firstName.includes("a");
-// value = firstName.concat(" ", lastName, " ", age);
-// value =
-//   "İsim: " +
-//   firstName +
-//   " " +
-//   "\nSoyisim: " +
-//   lastName +
-//   " " +
-//   "\nYaş: " +
-//   age +
-//   " " +
-//   "\nBölüm: " +
-//   department;
+value = firstName.length;
+value = lastName.toLowerCase();
+value = firstName.toUpperCase();
+value = firstName[0];
+value = firstName[firstName.length - 1];
+value = firstName[-1];
+
+value =
+  "İsim: " +
+  firstName +
+  " " +
+  "\nSoyisim: " +
+  lastName +
+  " " +
+  "\nYaş: " +
+  age +
+  " " +
+  "\nBölüm: " +
+  department;
+
+value = firstName.includes("a");//var mı yok mu kontrolü
+value = firstName.concat(" ", lastName, " ", age); //birleştirme
+value = firstName.charAt(5) // 5. indexteki karakteri verir.
+value = firstName.charCodeAt(4) // 5. indexteki karakterin ASCII tablosuna göre karşılığını verir. => a == 97
+value = firstName.endsWith("n", 6) // şununla bitiyor mu kontrolü?.
+value=String.fromCharCode(97) //ASCII'yi normale dönderir.
+value = firstName.indexOf("a"); //bulunduğu indexi verir
+value = firstName.lastIndexOf("a"); //Sondan başa doğru kontrol eder =>4
+value = firstName.repeat(3);//FurkanFurkanFurkan
+value = firstName.replace("an","iş"); //İlk eşleşmeyi bulur ve değiştirir.
+value = firstName.replaceAll("an","iş");//Tüm eşleşmeleri bulur ve değiştirir.
+value = firstName.slice(-6,-1);//İstenilen indexten sonrası ya da aralığı alır 
+value = firstName.split("");//İfadeyi, istenilen karaktere rastladıkça böler(şu an karakterlere böler)
+value = firstName.trim();//Baştaki ve sondaki boşlukları siler
 
 // Template Literal
 // value = `İsim: ${firstName}
@@ -40,15 +53,15 @@ const department = "Bilişim";
 // `;
 // value = `${firstName} ${lastName} ${age} ${department}`;
 
-value = `
-<ul>
-  <li>İsim: ${firstName}</li>
-  <li>Soyisim: ${lastName}</li>
-  <li>Yaş: ${age}</li>
-  <li>Bölüm: ${department}</li>
-</ul>
-`;
+// value = `
+// <ul>
+//   <li>İsim: ${firstName}</li>
+//   <li>Soyisim: ${lastName}</li>
+//   <li>Yaş: ${age}</li>
+//   <li>Bölüm: ${department}</li>
+// </ul>
+// `;
 
-document.body.innerHTML = value;
+// document.body.innerHTML = value;
 
 console.log(value);
