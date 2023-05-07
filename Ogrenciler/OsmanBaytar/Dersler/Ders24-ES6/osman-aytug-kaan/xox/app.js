0fd1589fea1628ecc98b33be5828056bcbd709ee
Aytug <yesilyurt.aytug@hotmail.com>
2023-05-07 20:32:16 +0300

    work

--- Ogrenciler/OsmanBaytar/Dersler/Ders24-ES6/osman-aytug-kaan/xox/app.js
+++ Ogrenciler/OsmanBaytar/Dersler/Ders24-ES6/osman-aytug-kaan/xox/app.js
@@ -18,30 +18,30 @@ const reset = document.getElementById("reset");
 
 let player = "x";
 let count = 0;
-
+let isEasyBot = false;
 
 function isPlayer() {
-    versusAI.classList.add("d-none");
-    startGame();
+	versusAI.classList.add("d-none");
+	startGame();
 }
 
 function isAI() {
-    versusPlayer.classList.add("d-none");
-    versusAI.classList.add("d-none");
-    easy.classList.replace("d-none", "initial");
-    hard.classList.replace("d-none", "initial");
+	versusPlayer.classList.add("d-none");
+	versusAI.classList.add("d-none");
+	easy.classList.replace("d-none", "initial");
+	hard.classList.replace("d-none", "initial");
 }
 
 function isEasy() {
-    hard.classList.replace("initial", "d-none");
-    startGame();
+	hard.classList.replace("initial", "d-none");
+	isEasyBot = true;
+	startGame();
 }
 
 function isHard() {
-    easy.classList.replace("initial", "d-none");
-    startGame();
+	easy.classList.replace("initial", "d-none");
+	startGame();
 }
-
 
 versusPlayer.addEventListener("click", isPlayer);
 versusAI.addEventListener("click", isAI);
@@ -49,117 +49,132 @@ easy.addEventListener("click", isEasy);
 hard.addEventListener("click", isHard);
 
 function write(e) {
-    if (player == "x") {
-        e.target.innerText = "X";
-        count = count + 1;
-        switchPlayer();
-        switchPlayerDisplay();
-        checkForWin();
-        checkForTile();
-        document.getElementById(e.target.id).removeEventListener("click", write);
-    }
-    else {
-        e.target.innerText = "O";
-        count = count + 1;
-        switchPlayer();
-        switchPlayerDisplay();
-        checkForWin();
-        checkForTile();
-        document.getElementById(e.target.id).removeEventListener("click", write);
-    }
+	if (player == "x") {
+		e.target.innerText = "X";
+		count = count + 1;
+		switchPlayer();
+		switchPlayerDisplay();
+		checkForWin();
+		checkForTile();
+		document.getElementById(e.target.id).removeEventListener("click", write);
+		document.getElementById(e.target.id).click();
+	} else {
+		if (isEasyBot) {
+			console.log("easy bot çalıştı");
+			easyAI();
+		} else {
+			e.target.innerText = "O";
+		}
+		count = count + 1;
+		switchPlayer();
+		switchPlayerDisplay();
+		checkForWin();
+		checkForTile();
+		document.getElementById(e.target.id).removeEventListener("click", write);
+	}
+}
+
+function easyAI() {
+	let random = Math.floor(Math.random() * 9) + 1;
+	while (true) {
+		if (document.getElementById(random).innerText == "") {
+			document.getElementById(random).innerText = "O";
+			console.log(random);
+			break;
+		}
+		random = Math.floor(Math.random() * 9) + 1;
+	}
 }
 
 function switchPlayerDisplay() {
-    if (player == "x") {
-        playerTurn.innerText = "Next Player is X"
-    }
-    else {
-        playerTurn.innerText = "Next Player is O"
-    }
+	if (player == "x") {
+		playerTurn.innerText = "Next Player is X";
+	} else {
+		playerTurn.innerText = "Next Player is O";
+	}
 }
 
 function switchPlayer() {
-    if (player == "x") {
-        player = "o";
-    }
-    else {
-        player = "x";
-    }
+	if (player == "x") {
+		player = "o";
+	} else {
+		player = "x";
+	}
 }
 
 function checkForWin() {
-    let winner = checkForRoW() || checkForColumn() || checkForDiagonal()
-    if (winner) {
-        winnerBox.innerText = `Winner = Player ${winner}`;
-        finishGame();
-    }
+	let winner = checkForRoW() || checkForColumn() || checkForDiagonal();
+	if (winner) {
+		winnerBox.innerText = `Winner = Player ${winner}`;
+		finishGame();
+	}
 }
 
 function checkForRoW() {
-    if (boxValue1.innerText == boxValue2.innerText && boxValue2.innerText == boxValue3.innerText) {
-        return boxValue1.innerText;
-    }
-    if (boxValue4.innerText == boxValue5.innerText && boxValue5.innerText == boxValue6.innerText) {
-        return boxValue4.innerText;
-    }
-    if (boxValue7.innerText == boxValue8.innerText && boxValue8.innerText == boxValue9.innerText) {
-        return boxValue7.innerText;
-    }
+	if (boxValue1.innerText == boxValue2.innerText && boxValue2.innerText == boxValue3.innerText) {
+		return boxValue1.innerText;
+	}
+	if (boxValue4.innerText == boxValue5.innerText && boxValue5.innerText == boxValue6.innerText) {
+		return boxValue4.innerText;
+	}
+	if (boxValue7.innerText == boxValue8.innerText && boxValue8.innerText == boxValue9.innerText) {
+		return boxValue7.innerText;
+	}
 }
 
 function checkForColumn() {
-    if (boxValue1.innerText == boxValue4.innerText && boxValue4.innerText == boxValue7.innerText) {
-        return boxValue1.innerText;
-    }
-    if (boxValue2.innerText == boxValue5.innerText && boxValue5.innerText == boxValue8.innerText) {
-        return boxValue2.innerText;
-    }
-    if (boxValue3.innerText == boxValue6.innerText && boxValue6.innerText == boxValue9.innerText) {
-        return boxValue3.innerText;
-    }
+	if (boxValue1.innerText == boxValue4.innerText && boxValue4.innerText == boxValue7.innerText) {
+		return boxValue1.innerText;
+	}
+	if (boxValue2.innerText == boxValue5.innerText && boxValue5.innerText == boxValue8.innerText) {
+		return boxValue2.innerText;
+	}
+	if (boxValue3.innerText == boxValue6.innerText && boxValue6.innerText == boxValue9.innerText) {
+		return boxValue3.innerText;
+	}
 }
 
 function checkForDiagonal() {
-    if (boxValue1.innerText == boxValue5.innerText && boxValue5.innerText == boxValue9.innerText) {
-        return boxValue1.innerText;
-    }
-    if (boxValue3.innerText == boxValue5.innerText && boxValue5.innerText == boxValue7.innerText) {
-        return boxValue3.innerText;
-    }
+	if (boxValue1.innerText == boxValue5.innerText && boxValue5.innerText == boxValue9.innerText) {
+		return boxValue1.innerText;
+	}
+	if (boxValue3.innerText == boxValue5.innerText && boxValue5.innerText == boxValue7.innerText) {
+		return boxValue3.innerText;
+	}
 }
 
 function checkForTile() {
-    if (count == 9 && !checkForWin()) {
-        winnerBox.innerText = "The game is Tile"
-    }
+	if (count == 9 && !checkForWin()) {
+		winnerBox.innerText = "The game is Tile";
+	}
 }
 
 function resetGame() {
-    window.location.reload();
+	window.location.reload();
 }
 
 reset.addEventListener("click", resetGame);
 
 function startGame() {
-    boxValue1.addEventListener("click", write);
-    boxValue2.addEventListener("click", write);
-    boxValue3.addEventListener("click", write);
-    boxValue4.addEventListener("click", write);
-    boxValue5.addEventListener("click", write);
-    boxValue6.addEventListener("click", write);
-    boxValue7.addEventListener("click", write);
-    boxValue8.addEventListener("click", write);
-    boxValue9.addEventListener("click", write);
+	boxValue1.addEventListener("click", write);
+	boxValue2.addEventListener("click", write);
+	boxValue3.addEventListener("click", write);
+	boxValue4.addEventListener("click", write);
+	boxValue5.addEventListener("click", write);
+	boxValue6.addEventListener("click", write);
+	boxValue7.addEventListener("click", write);
+	boxValue8.addEventListener("click", write);
+	boxValue9.addEventListener("click", write);
 }
 
 function finishGame() {
-    boxValue1.removeEventListener("click", write);
-    boxValue2.removeEventListener("click", write);
-    boxValue3.removeEventListener("click", write);
-    boxValue4.removeEventListener("click", write);
-    boxValue5.removeEventListener("click", write);
-    boxValue6.removeEventListener("click", write);
-    boxValue7.removeEventListener("click", write);
-    boxValue8.removeEventListener("click", write);
-    boxValue9.removeEventListener("click", write);
+	boxValue1.removeEventListener("click", write);
+	boxValue2.removeEventListener("click", write);
+	boxValue3.removeEventListener("click", write);
+	boxValue4.removeEventListener("click", write);
+	boxValue5.removeEventListener("click", write);
+	boxValue6.removeEventListener("click", write);
+	boxValue7.removeEventListener("click", write);
+	boxValue8.removeEventListener("click", write);
+	boxValue9.removeEventListener("click", write);
 }
