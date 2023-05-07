b5fe76558b4ba727a7df9990f191d885c6777eac
osmancankucuk <76538726+osmancankucuk@users.noreply.github.com>
2023-05-07 03:19:24 +0300

    player-player-added

---
+++ Ogrenciler/osmancankucuk/exams/Group/Group-12-osmancan-omer-ege/playerVsPlayer.js
@@ -0,0 +1,33 @@
+let curPlayer = document.querySelector(".currentPlayer");
+let gameArea = document.querySelector("gameArea");
+let box = document.querySelectorAll(".box");
+let chooseType = document.querySelector("#chooseType");
+import { winControll } from "./app.js";
+
+export let player = "X";
+
+curPlayer.textContent = `${player} Start`;
+
+export function playerVsPlayer() {
+  box.forEach((item) => {
+    item.addEventListener("click", () => paintArea(item));
+  });
+}
+export function setPlayerX() {
+  player = "X";
+}
+function paintArea(area) {
+  console.log(area);
+  if (area.textContent === "") {
+    area.textContent = player;
+    player === "X" ? (player = "O") : (player = "X");
+    winControll();
+    curPlayer.textContent = `Player ${player}`;
+  } else {
+    area.style.border = "1px solid red";
+    setTimeout(() => {
+      console.log("asd");
+      area.style.border = "1px solid gray";
+    }, 1500);
+  }
+}
