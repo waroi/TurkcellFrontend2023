14a3f4f44ba0f07041d2d376bb37fadfe04ccb9d
Burakhan Katdar <burakhankatdarr@gmail.com>
2023-05-08 18:18:48 +0300

    oyun tamamlandı

--- Ogrenciler/Burakhan-Katdar/Odevler/Genel/XOX/script.js
+++ Ogrenciler/Burakhan-Katdar/Odevler/Genel/XOX/script.js
@@ -1,5 +1,3 @@
-td = document.getElementsByTagName("td");
-
 currentState = "X";
 
 let XOX = [
@@ -9,7 +7,6 @@ let XOX = [
 ];
 let count = 0;
 let winner = false;
-let isBoardFull = true;
 
 document.querySelectorAll("td").forEach((td) => {
   td.addEventListener("click", (e) => {
@@ -21,18 +18,19 @@ document.querySelectorAll("td").forEach((td) => {
       let row = e.target.id[0];
       let col = e.target.id[2];
       XOX[row][col] = currentState;
-      console.table(XOX);
       currentState = currentState == "X" ? "O" : "X";
       count++;
+      let draw;
       for (let i = 0; i < 3; i++) {
-        checkWinner(XOX[i][0], XOX[i][1], XOX[i][2]);
-        checkWinner(XOX[0][i], XOX[1][i], XOX[2][i]);
-        
+        draw = checkWinner(XOX[i][0], XOX[i][1], XOX[i][2]);
+
+        draw = checkWinner(XOX[0][i], XOX[1][i], XOX[2][i]);
       }
-      
-      checkWinner(XOX[0][0], XOX[1][1], XOX[2][2]);
-      checkWinner(XOX[0][2], XOX[1][1], XOX[2][0]);
-      
+
+      draw = checkWinner(XOX[0][0], XOX[1][1], XOX[2][2]);
+      draw = checkWinner(XOX[0][2], XOX[1][1], XOX[2][0]);
+
+      draw == true ? alert("DRAW"): console.log("")
     }
   });
 });
@@ -41,38 +39,13 @@ function checkWinner(a, b, c) {
   if (a !== "" && a === b && b === c) {
     winner = true;
     console.log("Kazanan " + a);
+    alert("Kazanan " + a)
+    table.classList.add("nonClick");
+    return false
+  } else if (count == 9 && winner == false ) {
+    
+   
+    return true
   }
   
-  // for (let i = 0; i < XOX.length-1; i++) {
-
-  //     for (let j = 0; j < XOX[i].length-1; j++){
-  //         if(XOX[i][j] == XOX[i+1][j+1] && XOX[i][j] != ''){
-  //             winner = true
-  //             console.log("Kazanan" +XOX[i][j])
-  //         }
-  //     }
-
-  //     if( XOX[i][0] != '' && XOX[i][0] == XOX[i][1] && XOX[i][0] == XOX[i][2]   ){
-  //         winner = true
-  //         console.log("Kazanan " + XOX[i][0])
-
-  //     }
-  //     if( XOX[0][i] != '' && XOX[0][i] == XOX[1][i] && XOX[0][i] == XOX[2][i]   ){
-  //         winner = true
-  //         console.log("Kazanan " + XOX[0][i])
-  //     }
-
-  //     if( XOX[0][0] != '' && XOX[0][0] == XOX[1][1] && XOX[0][0] == XOX[2][2]   ){
-  //         winner = true
-  //         console.log("Kazanan " + XOX[0][0])
-  //     }
-  //     if( XOX[0][0] != '' && XOX[0][2] == XOX[1][1] && XOX[0][2] == XOX[2][0]   ){
-  //         winner = true
-  //         console.log("Kazanan " + XOX[0][2])
-  //     }
-
-  // }
-  // if(count == 9 && winner == false){
-  //     console.log("DRAW")
-  // }
 }
