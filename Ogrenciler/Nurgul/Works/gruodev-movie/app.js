29a387594c1f1f6bb14b8b485d610eefaaf63ce9
nurgulersan <nersan1998@gmail.com>
2023-05-10 22:07:00 +0300

    movie-son

--- Ogrenciler/Nurgul/Works/gruodev-movie/app.js
+++ Ogrenciler/Nurgul/Works/gruodev-movie/app.js
@@ -1,5 +1,4 @@
-// UI Constructor
-function UI() {}
+function UI() { }
 
 UI.prototype.addMovieToList = function (movie) {
   const movieTable = document
@@ -16,11 +15,26 @@ UI.prototype.addMovieToList = function (movie) {
     <td><a href="${movie.posterUrl}" target="_blank">Filmin posteri</a></td>
     <td>
       <button class="btn btn-danger">Sil</button>
-      <button class="btn btn-success">Güncelle</button>
+      <button class="btn btn-primary">Güncelle</button>
     </td>
   `;
 
+  //   const movieCard = document.getElementById("movieCard");
+  //   const card = document.createElement("div");
+  //   card.className="deneme";
+
+  //   card.innerHTML = `<img src="${movie.posterUrl}" class="card-img-top" alt="...">
+  //   <div class="card-body">
+  //     <h5 class="card-title">${movie.movieName}</h5>
+  //     <p class="card-text">${movie.director}</p>
+  //     <p class="card-text">${movie.year}</p>
+  //     <p class="card-text">${movie.type}</p>
+  //     <button class="btn btn-danger">Sil</button>
+  //       <button class="btn btn-primary">Güncelle</button>
+  //   </div>`;
+  //   movieCard.appendChild(card);
   movieTable.appendChild(tr);
+
 };
 
 UI.prototype.clearFormFields = function () {
@@ -31,22 +45,11 @@ UI.prototype.clearFormFields = function () {
   document.getElementById("posterUrl").value = "";
 };
 
-UI.prototype.showAlert = function (message, className) {
-  const alertDiv = document.createElement("div");
-  alertDiv.className = `alert ${className}`;
-  alertDiv.appendChild(document.createTextNode(message));
-
-  const container = document.querySelector(".container");
-  const form = document.getElementById("movieForm");
-  container.insertBefore(alertDiv, form);
-
-  setTimeout(function () {
-    alertDiv.remove();
-  }, 3000);
-};
-
-UI.prototype.deleteMovieFromList = function (target) { //???
+UI.prototype.deleteMovieFromList = function (target) {
   if (target.classList.contains("btn-danger")) {
+    target.parentElement.parentElement.remove();
+  }
+  else if (target.classList.contains("btn-primary")) {
     target.parentElement.parentElement.remove();
   }
 };
@@ -58,26 +61,8 @@ UI.prototype.updateMovieForm = function (movie) {
   document.getElementById("type").value = movie.type;
   document.getElementById("posterUrl").value = movie.posterUrl;
 };
-UI.prototype.updateMovieInList = function (index, movie) {
-  const movieRows = document.querySelectorAll("#movieTable tbody tr");
-  const movieRow = movieRows[index];
 
-  // Film bilgilerini güncelle
-  movieRow.cells[0].textContent = movie.movieName;
-  movieRow.cells[1].textContent = movie.director;
-  movieRow.cells[2].textContent = movie.year;
-  movieRow.cells[3].textContent = movie.type;
-
-  const posterUrlLink = movieRow.cells[4].querySelector("a");
-  posterUrlLink.href = movie.posterUrl;
-  
-  // Başarılı güncelleme mesajı göster
-  this.showAlert("Film başarıyla güncellendi", "alert-success");
-};
-
-
-// Film Constructor
-function Film(movieName, director, year, type, posterUrl) {
+function Movie(movieName, director, year, type, posterUrl) {
   this.movieName = movieName;
   this.director = director;
   this.year = year;
@@ -85,8 +70,7 @@ function Film(movieName, director, year, type, posterUrl) {
   this.posterUrl = posterUrl;
 }
 
-// Storage Constructor
-function Storage() {}
+function Storage() { }
 
 Storage.prototype.getMoviesFromStorage = function () {
   let movies;
@@ -96,7 +80,6 @@ Storage.prototype.getMoviesFromStorage = function () {
   } else {
     movies = [];
   }
-
   return movies;
 };
 
@@ -108,14 +91,13 @@ Storage.prototype.addMovieToStorage = function (movie) {
   localStorage.setItem("movies", JSON.stringify(movies));
 };
 
-Storage.prototype.updateMovieInStorage = function (index, movie) {
-  const movies = this.getMoviesFromStorage();
+//   const movies = this.getMoviesFromStorage();
 
-  movies.splice(index, 1, movie);
+//   movies.splice(index, 1, movie);
 
-  localStorage.setItem("movies", JSON.stringify(movies));
-  
-};
+//   localStorage.setItem("movies", JSON.stringify(movies));
+
+// };
 
 Storage.prototype.deleteMovieFromStorage = function (index) {
   const movies = this.getMoviesFromStorage();
@@ -127,75 +109,51 @@ Storage.prototype.deleteMovieFromStorage = function (index) {
 const ui = new UI();
 const storage = new Storage();
 
-// Form gönderildiğinde çalışacak fonksiyon
 function addMovie(event) {
-  event.preventDefault(); // Formun sayfayı yenilemesini engelliyor
+  event.preventDefault();
 
-  // Formdaki verileri al
   const movieName = document.getElementById("movieName").value;
   const director = document.getElementById("director").value;
   const year = document.getElementById("year").value;
   const type = document.getElementById("type").value;
   const posterUrl = document.getElementById("posterUrl").value;
 
-  // Yeni filmi oluştur
-  const movie = new Film(movieName, director, year, type, posterUrl);
+  const movie = new Movie(movieName, director, year, type, posterUrl);
 
-  // UI nesnesi üzerinden filmi tabloya ekle
   ui.addMovieToList(movie);
 
-  // Storage nesnesi üzerinden filmi localStorage'e ekle
   storage.addMovieToStorage(movie);
 
-  // Formu temizle
   ui.clearFormFields();
-
-  // Başarılı ekleme mesajı göster
-  ui.showAlert("Film başarıyla eklendi", "alert-success");
 }
 
-// Formu submit eventine bağla
-document.getElementById("movieForm").addEventListener("submit", addMovie);
-// Silme ve güncelleme butonlarına click eventi ekleme
-document.getElementById("movieTable").addEventListener("click", function (event) {
+function deleteAndUpdateClick(event) {
   // Silme butonuna tıklandığında
   if (event.target.classList.contains("btn-danger")) {
-    const rowIndex = event.target.parentElement.parentElement.rowIndex - 1;
+    const rowIndex = event.target.parentElement.parentElement.rowIndex;
 
-    // UI nesnesi üzerinden filmi tablodan sil
     ui.deleteMovieFromList(event.target);
 
-    // Storage nesnesi üzerinden filmi localStorage'dan sil
-    storage.deleteMovieFromStorage(rowIndex);
-
-    // Başarılı silme mesajı göster
-    ui.showAlert("Film başarıyla silindi", "alert-success");
+    storage.deleteMovieFromStorage(rowIndex - 1);
   }
-
   // Güncelleme butonuna tıklandığında
-  if (event.target.classList.contains("btn-success")) {
+  if (event.target.classList.contains("btn-primary")) {
     const rowIndex = event.target.parentElement.parentElement.rowIndex - 1;
-    ui.deleteMovieFromList(event.target);
-    // Storage nesnesi üzerinden güncellenecek filmi al
+
     const movies = storage.getMoviesFromStorage();
     const movie = movies[rowIndex];
-    
-    // UI nesnesi üzerinden güncelleme formunu doldur
+
     ui.updateMovieForm(movie);
-
-    // Storage nesnesi üzerinden filmi localStorage'dan sil
+    ui.deleteMovieFromList(event.target);
     storage.deleteMovieFromStorage(rowIndex);
-    
-    ui.updateMovieInList(rowIndex, movie);
-
-    // Başarılı güncelleme mesajı göster
-    ui.showAlert("Film güncelleme formuna yüklendi", "alert-info");
   }
 
-
   event.preventDefault();
-});
-// Sayfa yüklendiğinde localStorage'den verileri al ve tabloya ekle
+}
+document.getElementById("movieForm").addEventListener("submit", addMovie);
+
+document.getElementById("movieTable").addEventListener("click", deleteAndUpdateClick);
+
 document.addEventListener("DOMContentLoaded", function () {
   const movies = storage.getMoviesFromStorage();
 
@@ -203,6 +161,3 @@ document.addEventListener("DOMContentLoaded", function () {
     ui.addMovieToList(movie);
   });
 });
-
-
-
