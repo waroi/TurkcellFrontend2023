3df4d6fa112250900aad6af02c8e754d6776838a
Mirhat Fidan <mirhatfidann@gmail.com>
2023-05-13 23:18:20 +0300

    start

---
+++ Ogrenciler/MirhatFidan/Odevler/Ödev-2-Kütüphane/library.js
@@ -0,0 +1,8 @@
+function Book (name, writer, category, releaseDate, coverURL){
+    this.id = Date.now();
+    this.name = name;
+    this.writer = writer;
+    this.category = category;
+    this.releaseDate = releaseDate;
+    this.coverURL = coverURL;
+}
