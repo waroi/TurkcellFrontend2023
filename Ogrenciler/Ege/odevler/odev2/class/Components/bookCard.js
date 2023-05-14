83031107f10f2a8c4edcc6f6b98afa84992ceba0
Ege <karaege98@gmail.com>
2023-05-14 21:41:38 +0300

    Update bookCard.js

--- Ogrenciler/Ege/odevler/odev2/class/Components/bookCard.js
+++ Ogrenciler/Ege/odevler/odev2/class/Components/bookCard.js
@@ -4,10 +4,10 @@ function createCard(book) {
     <div class="card h-100">
     <img src="${book.coverURL}" class="card-img-top" alt="..." >
     <div class="card-body h-50">
-      <h2 class="book-title text-center">${book.name}</h2>
-      <h3 class="book-writer">${book.writer}</h3>
-      <h3 class="book-category">${book.category}</h3>
-      <h5 class="book-release-date text-end">${book.releaseDate}</h5>
+    <h2 class="book-title text-center">${book.name}</h2>
+    <h3 class="book-writer">Author: ${book.writer}</h3>
+    <h3 class="book-category">Category: ${book.category}</h3>
+    <h5 class="book-release-date text-end">${book.releaseDate}</h5>
       
       <div class="card-buttons text-center">
       <a href="#" data-bs-toggle="modal" data-bs-target="#addModal" class="btn btn-warning book-edit"><i class="fa-solid fa-pen-nib"></i></a>
