3ad4a204de4e4229fcd62db90858a8dc392c5eea
Ege <karaege98@gmail.com>
2023-05-14 21:31:16 +0300

    ui file classified

--- Ogrenciler/Ege/odevler/odev2/class/Classes/ui.js
+++ Ogrenciler/Ege/odevler/odev2/class/Classes/ui.js
@@ -11,46 +11,50 @@ const bookCoverInp = document.querySelector("#book-cover");
 const addBookBtn = document.querySelector("#add-submit");
 const editBookBtn = document.querySelector("#edit-submit");
 
-export function UI(display) {
-  this.display = display;
+class UI {
+  constructor(display) {
+    this.display = display;
+  }
 
-  this.updateDisplay = function (books) {
+  updateDisplay(books) {
     this.display.innerHTML = "";
     books.map((book) => (this.display.innerHTML += createCard(book)));
-  };
+  }
 
-  this.uniqueCategories = function (books) {
+  uniqueCategories(books) {
     const categoriesSet = new Set(books.map((book) => book.category));
     categorySelect.innerHTML = "";
     categorySelect.innerHTML += `<option value="">All</option>`;
     categorySelect.innerHTML += Array.from(categoriesSet).map((category) => {
       return createOption(category);
     });
-  };
+  }
 
-  this.uniqueWriters = function (books) {
+  uniqueWriters(books) {
     const writersSet = new Set(books.map((book) => book.writer));
     writerSelect.innerHTML = "";
     writerSelect.innerHTML += `<option value="">All</option>`;
     writerSelect.innerHTML += Array.from(writersSet).map((writer) => {
       return createOption(writer);
     });
-  };
+  }
 
-  this.makeUniques = function (books) {
-    this.uniqueCategories(books);
-    this.uniqueWriters(books);
-  };
+  makeUniques(books) {
+    uniqueCategories(books);
+    uniqueWriters(books);
+  }
+
+  static cardToModal(books, bookID) {
+    UI.prototype.cardToModal = function (books, bookID) {
+      const book = books.find((book) => book.id == bookID);
+      console.log(bookID);
+      bookNameInp.value = book.name;
+      bookWriterInp.value = book.writer;
+      bookCategoryInp.value = book.category;
+      bookDateInp.value = book.releaseDate;
+      bookCoverInp.value = book.coverURL;
+      addBookBtn.classList.toggle("hidden");
+      editBookBtn.classList.toggle("hidden");
+    };
+  }
 }
-
-UI.prototype.cardToModal = function (books, bookID) {
-  const book = books.find((book) => book.id == bookID);
-  console.log(bookID);
-  bookNameInp.value = book.name;
-  bookWriterInp.value = book.writer;
-  bookCategoryInp.value = book.category;
-  bookDateInp.value = book.releaseDate;
-  bookCoverInp.value = book.coverURL;
-  addBookBtn.classList.toggle("hidden");
-  editBookBtn.classList.toggle("hidden");
-};
