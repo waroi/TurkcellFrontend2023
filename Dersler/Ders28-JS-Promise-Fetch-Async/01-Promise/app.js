3cf1932b9fb4b1d8b0163937bc72811451c3a056
Varol Maksutoglu <varolmaksutoglu@yandex.com>
2023-05-17 19:04:05 +0300

    promise

---
+++ Dersler/Ders28-JS-Promise-Fetch-Async/01-Promise/app.js
@@ -0,0 +1,17 @@
+function getData(data) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      if (typeof data === "string") {
+        console.log("olumlu");
+        resolve("Datayı Aldık");
+      } else {
+        console.log("olumsuz");
+        reject("Datayı Alamadık");
+      }
+    }, 3000);
+  });
+}
+
+getData("test")
+  .then((response) => console.log(response))
+  .catch((error) => console.log(error));
