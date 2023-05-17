6e4085a265288ce104a846febdf434bf073e698b
Aytug <yesilyurt.aytug@hotmail.com>
2023-05-17 20:34:34 +0300

    update

--- Ogrenciler/aytug/front/furkan-ahmet/app.js
+++ Ogrenciler/aytug/front/furkan-ahmet/app.js
@@ -2,10 +2,9 @@ function getData() {
 	fetch("https://jsonplaceholder.typicode.com/users")
 		.then((response) => response.json())
 		.then((json) => {
-			let userList = json.map((user) => `<tr>${user.id}</tr><tr>${user.name}</tr>`);
-			console.log(userList);
+			let userList = json.map((user) => `<tr><td>${user.id}</td><td>${user.name}</td></tr>`);
 			let renderList = document.getElementById("userList");
-			renderList.innerHTML += userList;
+			renderList.innerHTML = userList.join("");
 		})
 		.catch((error) => console.log(error));
 }
