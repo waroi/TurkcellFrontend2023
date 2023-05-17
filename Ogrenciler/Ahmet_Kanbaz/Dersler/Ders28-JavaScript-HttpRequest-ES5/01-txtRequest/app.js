e682659c38468ad02034ad81de46344e9dfc9348
smartbreath <ahmetkanbaz@gmail.com>
2023-05-17 04:53:20 +0300

    Ders28-JavaScript-XMLRequest

--- Ogrenciler/Ahmet_Kanbaz/Dersler/Ders28-JavaScript-HttpRequest-ES5/01-txtRequest/app.js
+++ Ogrenciler/Ahmet_Kanbaz/Dersler/Ders28-JavaScript-HttpRequest-ES5/01-txtRequest/app.js
@@ -3,19 +3,19 @@ const outputData = document.getElementById('output');
 
 getDataButton.addEventListener('click', function() {
   const xhr = new XMLHttpRequest();
-  // console.log(xhr);
+  console.log(xhr);
 
   xhr.onreadystatechange = function() {
-    // console.log(this.readyState);
+    console.log(this.readyState);
 
-    // if(this.readyState === 4 && this.status === 200) {
-      // console.log(this.responseText);
-    //   outputData.textContent = this.responseText;
-    // }
-    // if(this.readyState === 4 && this.status === 404) {
-    //   console.log('Aradığınız Veriye Ulaşılamadı.');
-    //   outputData.textContent = 'Aradığınız Veriye Ulaşılamadı.';
-    // }
+    if(this.readyState === 4 && this.status === 200) {
+      console.log(this.responseText);
+      outputData.textContent = this.responseText;
+    }
+    if(this.readyState === 4 && this.status === 404) {
+      console.log('Aradığınız Veriye Ulaşılamadı.');
+      outputData.textContent = 'Aradığınız Veriye Ulaşılamadı.';
+    }
   };
 
   xhr.onload = function() {
