b33cfa431a6cbdc23c1a6a65d55076ce3fa33fde
Ege <karaege98@gmail.com>
2023-05-18 14:35:52 +0300

    midterm 3 add data to json-server

--- Ogrenciler/Ege/odevler/odev3/Classes/request.js
+++ Ogrenciler/Ege/odevler/odev3/Classes/request.js
@@ -1,11 +1,27 @@
 class Request {
   async get(url) {
     const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error("Bir hata oluştu");
-    }
+    if (!response.ok) throw new Error("Veri alımında bir hata oluştu");
+
     const data = await response.json();
     return data;
+  }
+
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error("Request failed");
+    }
+
+    const result = await response.json();
+    return result;
   }
 }
 
--- Ogrenciler/Ege/odevler/odev3/app.js
+++ Ogrenciler/Ege/odevler/odev3/app.js
@@ -2,7 +2,25 @@ import Request from "./Classes/request.js";
 
 const request = new Request();
 
-request
-  .get("http://localhost:3000/blogs")
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+const url = "http://localhost:3000/blogs";
+const postData = {
+  author: "mEge Karam",
+  title: "mEge-Merver",
+  text: "morem mipsum molor",
+};
+
+// request
+//   .get("http://localhost:3000/blogs")
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+addData.addEventListener("click", () => {
+  request
+    .post(url, postData)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
