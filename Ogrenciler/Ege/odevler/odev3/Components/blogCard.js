e744cd2bd0fab2b7f05a713cd527f206a97832dd
Ege <karaege98@gmail.com>
2023-05-20 16:00:13 +0300

    midterm3 styling update

--- Ogrenciler/Ege/odevler/odev3/Components/blogCard.js
+++ Ogrenciler/Ege/odevler/odev3/Components/blogCard.js
@@ -11,10 +11,10 @@ function blogCard(blog) {
             <div class="col-lg-8">
             <div class="card-body">
         <h5 class="card-title">${blog.title}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">by ${blog.author} at ${
+        <h6 class="card-subtitle mb-2">by ${blog.author} at ${
     blog.releaseDate
   } ${blog.releaseTime}</h6>
-        <h6 class="card-subtitle mb-2 text-muted">${blog.category}</h6>
+        <h6 class="card-subtitle mb-2">${blog.category}</h6>
         <p class="card-text">${blog.text.slice(0, 25)}...</p>
         <button
           type="button"
@@ -22,10 +22,10 @@ function blogCard(blog) {
           data-bs-toggle="modal"
           data-bs-target="#blog-modal"
         >
-          See in full
+        <i class="fa-brands fa-readme"></i>
         </button>
-        <button type="button" class="btn btn-warning edit-blog">Edit</button>
-        <button type="button" class="btn btn-danger delete-blog">Delete</button>
+        <button type="button" class="btn btn-warning edit-blog"><i class="fa-solid fa-pen-to-square"></i></button>
+        <button type="button" class="btn btn-danger delete-blog"><i class="fa-solid fa-trash"></i></button>
       </div>
       </div>
       </div>
