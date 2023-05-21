3eb0fd129811e0cca7507c3e1d8109f75eca3cd8
Selahattin Demir <selahattindemir686@gmail.com>
2023-05-22 00:40:20 +0300

    updated files

--- Ogrenciler/SelahattinDemir/Odevler/Odev-3/scripts/Components/musicCard.js
+++ Ogrenciler/SelahattinDemir/Odevler/Odev-3/scripts/Components/musicCard.js
@@ -2,7 +2,7 @@ class MusicCard {
   static addMusicFromMusicCard(music) {
     musicList.innerHTML += `
     <div class="col-md-6 col-lg-3 my-5 px-3 blog-card" id="${music.id}">
-      <div class="d-flex align-items-center justify-content-center card-img ms-0 ms-lg-3">
+      <div class="d-flex align-items-center justify-content-center card-img ms-5 ms-lg-3">
         <a data-bs-toggle="modal" data-bs-target="#a${music.id}">
           <picture class="position-relative d-block">
             <source
@@ -26,9 +26,11 @@ class MusicCard {
         </a>
       </div>
       <div class="card-body card-shadow p-3 mt-4">
+      <a data-bs-toggle="modal" data-bs-target="#a${music.id}">
         <h3 class="card-title music-name text-light">${music.name}</h3>
         <h6 class="text-center text-success fs-5">${music.writer}</h6>
         <p class="card-text overflow-hidden text-light">${music.textContent}</p>
+        </a>
         <p class="card-text text-truncate text-light">${music.category}</p>
         <h5 class="text-end fs-6 text-light">${music.date}</h5>
         <h5 class="text-end fs-6 text-light">${music.clock}</h5>
