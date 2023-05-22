37b67f61d979380c36419978894200be11c910d5
mfurkanuygur <u.fe.uygur@gmail.com>
2023-05-22 21:36:34 +0200

    react intro

---
+++ Ogrenciler/MFurkanUygur/DersNotlarim/Gun28(ReactGiris)/ilk-app/src/deneme.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function deneme() {
+  return (
+    <div>Sadness</div>
+  )
+}
+
+//rfc veya rfce => function component
+//rcc =>class component
+//rafc veya rafce => arrow function component
