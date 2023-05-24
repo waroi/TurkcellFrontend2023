a8792bf8cde1e2cd8d41a21aedbc1c7e11eb3ee6
07kagan07 <kagancoskun32@gmail.com>
2023-05-24 03:25:03 +0300

    ders29

---
+++ Ogrenciler/KaganCoskun/GrupCalismalari/Ders29-React-Giris/ilk-proje/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Deneme from './Deneme';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        merhaba
+        <Deneme />
+      </header>
+    </div>
+  );
+}
+
+export default App;
