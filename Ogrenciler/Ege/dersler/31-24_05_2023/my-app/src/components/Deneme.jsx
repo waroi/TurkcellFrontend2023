61c8e887b1eaa1091c81f78ec6db5fce5e7b0253
Ege <karaege98@gmail.com>
2023-05-24 22:16:38 +0300

    Lesson 31 - React Components

---
+++ Ogrenciler/Ege/dersler/31-24_05_2023/my-app/src/components/Deneme.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Deneme = (/* props */ { isim }) => {
+  //   return <div>Merhaba {props.isim}</div>;
+  return <div>Merhaba {isim}</div>;
+};
+
+export default Deneme;
