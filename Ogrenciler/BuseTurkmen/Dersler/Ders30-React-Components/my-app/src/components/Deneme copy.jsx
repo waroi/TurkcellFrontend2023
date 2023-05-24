b0e51a01ef0170ace50ea4a5c62c1069078712a9
BuseTurkmen <buse.turkmen2000@gmail.com>
2023-05-24 22:35:44 +0300

    Ders30

---
+++ Ogrenciler/BuseTurkmen/Dersler/Ders30-React-Components/my-app/src/components/Deneme copy.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Deneme = ({ isim, surname, age }) => {
+  return (
+    <div>
+      Merhaba {isim} {surname}, {age} yasindasin
+    </div>
+  );
+};
+
+Deneme.propTypes = {
+  isim: PropTypes.string.isRequired,
+  surname: PropTypes.string.isRequired,
+  age: PropTypes.number,
+};
+
+Deneme.defaultProps = {
+  isim: "isim girilmedi",
+  surname: "soyisim girilmedi",
+};
+
+export default Deneme;
