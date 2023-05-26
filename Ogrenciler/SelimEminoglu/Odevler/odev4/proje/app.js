f31d094c93eb918f3174c991e1ba65d2f15f9b2b
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-05-26 11:53:35 +0300

    Odev Güncellemesi
    Odev için ekleme modalı düzenlendi ,fonskiyonlar kısmına başlandı.

--- Ogrenciler/SelimEminoglu/Odevler/odev4/proje/app.js
+++ Ogrenciler/SelimEminoglu/Odevler/odev4/proje/app.js
@@ -1,6 +1,46 @@
 const productList = document.getElementById("productList");
 const showİnfoModal = document.getElementById("showİnfoModal");
 
+const productName = document.getElementById("productName");
+const productPrice = document.getElementById("productPrice");
+const productCategory = document.getElementById("productCategory");
+const productDescription = document.getElementById("productType");
+const productCount = document.getElementById("productCount");
+const productİmage = document.getElementById("productİmage");
+
+const addProductButton = document.getElementById("addProduct");
+
+addProductButton.addEventListener("click", addProduct());
+
+function addProduct() {
+  let id = Date.now();
+
+  let newProduct = {
+    id: id,
+    name: productName.value,
+    price: productPrice.value,
+    category: productCategory.value,
+    description: productDescription.value,
+    count: productCount.value,
+    image: productİmage.value,
+  };
+
+  for (let key in newProduct) {
+    if (newProduct.hasOwnProperty(key)) {
+      if (key == "price" || key == "count") {
+        if (key < 0) {
+          alert("Lütfen Pozitif Sayı Giriniz");
+          return;
+        }
+      }
+      if (newProduct[key] == "") {
+        alert("Lütfen Boş Bırakmayınız");
+        return;
+      }
+    }
+  }
+}
+
 function showList() {
   Json.getProducts()
     .then((data) => {
