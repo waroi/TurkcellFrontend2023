29ceef7369c3bd72f7b9ce464a9da541c66d3f52
Ege <karaege98@gmail.com>
2023-05-29 21:53:50 +0300

    Lesson 33 - React

---
+++ Ogrenciler/Ege/dersler/33-29_05_2023/vite-react/src/components/Deneme.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+const Deneme = ({ isim }) => {
+  return <div>Merhaba {isim} </div>;
+};
+
+Deneme.propTypes = {
+  isim: PropTypes.string.isRequired,
+};
+
+export default Deneme;
