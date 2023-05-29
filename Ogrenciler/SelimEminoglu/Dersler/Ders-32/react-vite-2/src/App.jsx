f7c10270afdad30ed5d9436307b0f4a1d3fb81d1
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-05-29 21:39:24 +0300

    Ders İçeriği Eklemesi
    Ders-32 eklendi.

--- Ogrenciler/SelimEminoglu/Dersler/Ders-32/react-vite-2/src/App.jsx
+++ Ogrenciler/SelimEminoglu/Dersler/Ders-32/react-vite-2/src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Deneme from "./components/Deneme";
+import Listİtem from "./components/Listİtem";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // const [count, setCount] = useState(0);
+  const [name, setName] = useState("Selim");
+  const students = ["ayşe", "gülçin", "selim", "yavuz"];
+  const data = [
+    {
+      id: 1,
+      name: "Trabzonspor",
+      color1: "red",
+      color2: "blue",
+    },
+    {
+      id: 2,
+      name: "Rizespor",
+      color1: "green",
+      color2: "white",
+    },
+    {
+      id: 3,
+      name: "İstanbulspor",
+      color1: "black",
+      color2: "yellow",
+    },
+  ];
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      {/* <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </div> */}
+      <Deneme isim={name} />
+      <button
+        onClick={() => {
+          setName("yavuz");
+        }}
+      >
+        change name
+      </button>
+      <hr />
+      <ul>
+        {students.map((student, i) => (
+          <Listİtem key={i} student={student} />
+        ))}
+      </ul>
+      {data.map((team) => {
+        return (
+          <li
+            key={team.id}
+            style={{
+              background: team.color1,
+              color: team.color2,
+              width: "300px",
+            }}
+          >
+            {team.name}
+          </li>
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
