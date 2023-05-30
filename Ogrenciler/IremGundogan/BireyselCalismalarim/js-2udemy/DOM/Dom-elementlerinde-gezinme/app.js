9615162cbb0e694bc11dce051ac48c5489a38278
İrem Gündoğan <rem1035@hotmail.com>
2023-05-31 01:41:06 +0300

    ..

---
+++ Ogrenciler/IremGundogan/BireyselCalismalarim/js-2udemy/DOM/Dom-elementlerinde-gezinme/app.js
@@ -0,0 +1,16 @@
+let value;
+const todoList = document.querySelector(".list-group");
+const todo = document.querySelector(".list-group-item:nth-child(2)");
+const cardrow = document.querySelector(".card.row");
+
+value = todoList;
+value = todo;
+value = cardrow;
+
+
+//child nodes
+value=todoList.childNodes;
+value=todoList.childNodes[0];
+
+
+console.log(value);
---
+++ Ogrenciler/IremGundogan/BireyselCalismalarim/js-2udemy/DOM/Element-secme/app.js
@@ -0,0 +1,28 @@
+// //elementi id ye göre seçme
+// let element;
+// element = document.getElementById("todo-form");
+// element = document.getElementById("tasks-title");
+
+// //Element class a göre seçme
+// element = document.getElementsByClassName("list-group-item");
+// element = document.getElementsByClassName("card-header");
+
+// //Element tag'e göre seçme
+// element = document.getElementsByTagName("li");
+
+// //query selector - css selector-tek element dönüyor
+// element = document.querySelector("#todo-form");
+// element = document.querySelector("#tasks-title");
+
+// element = document.querySelector(".list.group-item");
+// element = document.querySelector("li");
+// element = document.querySelector("div");
+
+// //QuerySelectorAll- Tüm Elementleri Seçer
+// element =document.querySelectorAll(".list-group-item");
+
+// element.forEach(function (el) {
+//   console.log(el);
+// });
+
+// console.log(element);
