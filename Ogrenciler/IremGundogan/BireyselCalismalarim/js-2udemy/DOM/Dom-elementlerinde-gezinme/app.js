f9127a64f548e0c6ad6dc9248750552686044084
İrem Gündoğan <rem1035@hotmail.com>
2023-05-31 02:12:59 +0300

    ..

--- Ogrenciler/IremGundogan/BireyselCalismalarim/js-2udemy/DOM/Dom-elementlerinde-gezinme/app.js
+++ Ogrenciler/IremGundogan/BireyselCalismalarim/js-2udemy/DOM/Dom-elementlerinde-gezinme/app.js
@@ -1,16 +1,50 @@
-let value;
-const todoList = document.querySelector(".list-group");
-const todo = document.querySelector(".list-group-item:nth-child(2)");
-const cardrow = document.querySelector(".card.row");
+// let value;
+// const todoList = document.querySelector(".list-group");
+// const todo = document.querySelector(".list-group-item:nth-child(2)");
+// const cardrow = document.querySelector(".card.row");
 
-value = todoList;
-value = todo;
-value = cardrow;
-
-
-//child nodes
-value=todoList.childNodes;
-value=todoList.childNodes[0];
+// value = todoList;
+// value = todo;
+// value = cardrow;
 
 
-console.log(value);
+// //child nodes-text dahil
+// value=todoList.childNodes;
+// value=todoList.childNodes[0];
+
+
+// //Children methodu-sadece element
+
+// value=todoList.children;
+// value=todoList.children[0];
+// value=todoList.children[2].textContent="Değişti";
+
+
+// value=cardrow;
+// value=cardrow.children;
+// value=cardrow.children[2].children[1].textContent="Burası da değişti";
+
+
+// value=todoList;
+// value=todoList.children[0];
+// value=todoList.firstElementChild;
+// value=todoList.lastElementChild;
+// value=todoList.children.length;
+// value=todoList.childElementCount;
+
+
+// value=cardrow;
+// value=cardrow.parentElement;
+// value=cardrow.parentElement.parentElement;
+
+// //Element kardeşleri
+// value=todo;
+// value=todo.previousElementSibling;
+// value=todo.nextElementSibling;
+// value=todo.nextElementSibling.nextElementSibling;
+
+
+
+
+
+// console.log(value);
