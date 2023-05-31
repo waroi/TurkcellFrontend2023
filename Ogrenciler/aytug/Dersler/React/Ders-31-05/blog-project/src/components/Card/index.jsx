e4daf1b97ebc2edbc8d48c90c4fa97601eb3df8a
Aytug <yesilyurt.aytug@hotmail.com>
2023-05-31 22:38:33 +0300

    update

--- Ogrenciler/aytug/Dersler/React/Ders-31-05/blog-project/src/components/Card/index.jsx
+++ Ogrenciler/aytug/Dersler/React/Ders-31-05/blog-project/src/components/Card/index.jsx
@@ -1,11 +1,15 @@
 import PropTypes from "prop-types";
 
 const Card = ({ picsum, json }) => {
-	// let blogList = [picsum, ...json];
+	const mergedData = picsum.map((item1) => {
+		const item = json.find((item2) => item2.id == item1.id);
+		return { ...item1, ...item };
+	});
 
+	console.log(mergedData);
 	return (
 		<>
-			{picsum.map((item) => (
+			{mergedData.map((item) => (
 				<div key={item.id} className="card mb-3" style={{ maxWidth: "540px" }}>
 					<div className="row g-0">
 						<div className="col-md-4">
@@ -14,10 +18,7 @@ const Card = ({ picsum, json }) => {
 						<div className="col-md-8">
 							<div className="card-body">
 								<h5 className="card-title">{item.author}</h5>
-								<p className="card-text">
-									This is a wider card with supporting text below as a natural lead-in to additional content. This
-									content is a little bit longer.
-								</p>
+								<p className="card-text">{item.title}</p>
 								<p className="card-text">
 									<small className="text-body-secondary">Last updated 3 mins ago</small>
 								</p>
