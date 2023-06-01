fd5cb22265b80d53aaef3d9407fe587856d19e88
İrem Gündoğan <rem1035@hotmail.com>
2023-06-02 02:28:00 +0300

    ...

--- Ogrenciler/IremGundogan/BireyselCalismalarim/React/React-1/react-1/src/App.js
+++ Ogrenciler/IremGundogan/BireyselCalismalarim/React/React-1/react-1/src/App.js
@@ -2,13 +2,25 @@ import React from "react";
 import Navi from "./Navi";
 import CategoryList from "./CategoryList";
 import ProductList from "./ProductList";
-
+import { Container, Row, Col } from "react-bootstrap";
 function App() {
+  let titleProduct = "Product List";
+  let titleCategory = "Category List";
   return (
     <div>
-      <Navi />
-      <CategoryList />
-      <ProductList />
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList title={titleCategory} />
+          </Col>
+          <Col xs="9">
+            <ProductList title={titleProduct} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
