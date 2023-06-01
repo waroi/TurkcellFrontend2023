01c53471c6d3cf6b7ff04234f0a3bb6809216be7
BuseTurkmen <buse.turkmen2000@gmail.com>
2023-06-01 13:33:53 +0300

    ders33

---
+++ Ogrenciler/BuseTurkmen/Grup Çalışmaları/blog-react/blogpage/src/Components/CardBody/index.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import "./style.css";
+import BlogCard from '../Card';
+function CardBody() {
+  
+  
+  return (
+    <div>
+      {
+
+      }
+    </div>
+  )
+}
+
+export default CardBody
