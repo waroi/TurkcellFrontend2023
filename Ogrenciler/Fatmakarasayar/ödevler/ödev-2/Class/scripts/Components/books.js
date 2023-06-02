bc0f11dc7c43147d0c951a1249060497395ee02e
Fatma Karasayar <ftmkrsyr0q@gmail.com>
2023-06-02 18:02:38 +0300

    BİG-UPDATE

---
+++ Ogrenciler/Fatmakarasayar/ödevler/ödev-2/Class/scripts/Components/books.js
@@ -0,0 +1,10 @@
+class Books {
+  constructor(id, img, name, category, year, author) {
+    this.id = id;
+    this.img = img;
+    this.name = name;
+    this.category = category;
+    this.year = year;
+    this.author = author;
+  }
+}
