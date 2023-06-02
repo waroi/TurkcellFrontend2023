6673d30961bba97c1e7477cb64bd2134ccbf5696
busra4 <bkosnak10@hotmail.com>
2023-06-02 16:43:01 +0300

    changed files

--- Ogrenciler/BusraK/GrupCalismalari/guzin-busra-yasin/todoApp/src/components/Header.jsx
+++ Ogrenciler/BusraK/GrupCalismalari/guzin-busra-yasin/todoApp/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import { SiProtodotio } from "react-icons/si";
 const Header = () => {
   return (
     <div>
-      <h1>Todo App</h1>
+      <h1>
+        <SiProtodotio className="todo-icon" /> Todo App
+      </h1>
     </div>
   );
 };
