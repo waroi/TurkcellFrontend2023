df3068318e401bf22b39e2da9133dbcffcd4c3c2
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-06-02 21:31:16 +0300

    Grup Çalışması Eklemesi
    Kurulum yapıldı ve ilk component eklendi.

---
+++ Ogrenciler/SelimEminoglu/Toplantılar/Toplantı-34/github-user/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import SearchUser from "./components/Search_user/SearchUser";
+
+function App() {
+  return (
+    <>
+      <h1>Github User Founder</h1>
+      <SearchUser />
+    </>
+  );
+}
+
+export default App;
---
+++ Ogrenciler/SelimEminoglu/Toplantılar/Toplantı-34/github-user/src/components/Search_user/SearchUser.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import UserStyle from "./SearchUser.module.css";
+
+function SearchUser() {
+  const [user, setUser] = useState("");
+
+  const takeValue = (e) => {
+    setUser(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={UserStyle.formStyle}>
+      <input
+        type="text"
+        placeholder="Kullanıcı Adı Girin"
+        onChange={takeValue}
+        value={user}
+        className={`${UserStyle.formInput} form-control`}
+      />
+      <button className={UserStyle.formButton} type="submit">
+        Kullanıcı Ara
+      </button>
+    </form>
+  );
+}
+
+export default SearchUser;
