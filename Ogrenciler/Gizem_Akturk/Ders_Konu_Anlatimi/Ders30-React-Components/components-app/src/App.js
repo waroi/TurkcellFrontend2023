951a577975fb3cce4bd62674edebec7a39e6a75d
gizemakturk <gizemakturk.ga@gmail.com>
2023-06-04 15:44:00 +0300

    added some files to lesson 30

--- Ogrenciler/Gizem_Akturk/Ders_Konu_Anlatimi/Ders30-React-Components/components-app/src/App.js
+++ Ogrenciler/Gizem_Akturk/Ders_Konu_Anlatimi/Ders30-React-Components/components-app/src/App.js
@@ -1,21 +1,21 @@
-import "./App.css";
-// import Deneme from "./components/Deneme";
-import ListItem from "./components/ListItem";
+import './App.css';
+import Deneme from './components/Deneme'; 
+import ListItem from './components/ListItem';
 
 function App() {
-  // let name = "Varol";
-  // let surname = "Maksutoğlu";
-  // let age = "37";
-  const students = ["Varol", "Oğulcan", "Ege", "Aleyna", "Osman"];
+  let name ="Gizem";
+  let surname ="Aktürk";
+  let age = 25;
+  const students = ["Gizem","Tuna","Mehmet","Ayşe"]
   return (
     <div className="App">
       <header className="App-header">
-        {/* <Deneme isim={name} surname={surname} age={age} /> */}
+        <Deneme isim={name} surname={surname} age={age}/>
         <ul>
-          {students.map((student, index) => (
-            <ListItem key={index} student={student} />
-          ))}
+          {students.map((student ,index) => <ListItem key={index} student={student}/>)
+          }
         </ul>
+       
       </header>
     </div>
   );
