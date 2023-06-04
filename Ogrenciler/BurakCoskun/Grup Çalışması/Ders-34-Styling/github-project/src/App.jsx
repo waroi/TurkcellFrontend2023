a6f19ed1dcca58d845e707b4d6a84a98ddaa0263
Burak Coskun <92813347+retr0senss@users.noreply.github.com>
2023-06-04 19:46:07 +0300

    update

--- Ogrenciler/BurakCoskun/Grup Çalışması/Ders-34-Styling/github-project/src/App.jsx
+++ Ogrenciler/BurakCoskun/Grup Çalışması/Ders-34-Styling/github-project/src/App.jsx
@@ -8,24 +8,32 @@ function App() {
   const [searchValue, setSearchValue] = useState("");
   const [user, setUser] = useState({});
   const [lastSearches, setLastSearches] = useState([]);
+  // const [isSearch, setIsSearch] = useState(false);
+  const [data, setData] = useState("");
 
   
   const handleSearch = async () => {
-    await fetch(`https://api.github.com/users/${searchValue}`).then((res) =>
+    setData(!data && searchValue);
+    await fetch(`https://api.github.com/users/${data}`).then((res) =>
       res.json().then((data) => setUser(data))
     );
-    if (lastSearches.indexOf(searchValue) === -1) {
-      setLastSearches([...lastSearches, searchValue]);
+    if (lastSearches.indexOf(data) === -1) {
+      setLastSearches([...lastSearches, data]);
     }
   };
-  console.log(user);
+
+
   useEffect(()=>{
-console.log(searchValue)
-},[searchValue])
+    if (data){
+     handleSearch();
+    }
+},[data])
 
   const handleLastSearch =  async (lastSearch) => {
-     setSearchValue(lastSearch);
+     setData(lastSearch);
+    //  setIsSearch(true)
      await handleSearch();
+    // setIsSearch(false)
   }
 
   return (
@@ -34,12 +42,14 @@ console.log(searchValue)
         <div className={Styles.searchArea}>
           <h2>Github Kullanıcısı Arayın</h2>
           <p>Bir kullanıcı adı girin ve github bilgilerini görün!</p>
-          <input className={Styles.searchInput} type="text" onChange={(e) => setSearchValue(e.target.value)} />
+          <input id="input" className={Styles.searchInput} type="text" onChange={(e)=>setSearchValue(e.target.value)}/>
           <button className={Styles.searchButton} onClick={handleSearch}>Ara</button>
         </div>
         <LastSearches lastSearches={lastSearches} onLastSearchClick = {handleLastSearch} />
         <Profile user={user} />
-        <Repos username={user.login} />
+        {
+          user.name && <Repos username={user.login} /> 
+        }
       </div>
     </>
   );
