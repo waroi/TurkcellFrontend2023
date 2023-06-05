50cba63850611e5a5f656a44aac1b6d87e6cedc6
Aytug <yesilyurt.aytug@hotmail.com>
2023-06-05 03:36:13 +0300

    update

--- Ogrenciler/aytug/Dersler/GroupProjects/02-05(github)/react-styling/src/App.jsx
+++ Ogrenciler/aytug/Dersler/GroupProjects/02-05(github)/react-styling/src/App.jsx
@@ -1,24 +1,31 @@
 // import { useState } from 'react'
-import UserList from './components/UserList/UserList'
+import UserList from "./components/UserList/UserList";
 
-import './App.css'
-import UserSearch from './components/UserSearch/UserSearch'
-import { useState } from 'react'
+import "./App.css";
+import UserSearch from "./components/UserSearch/UserSearch";
+import { useState } from "react";
 
 function App() {
-  const [renderCheck, setRenderCheck] = useState(false)
-  const [activeUser, setActiveUser] = useState()
-  const [userRepos, setUserRepos] = useState([])
+	const [renderCheck, setRenderCheck] = useState(false);
+	const [activeUser, setActiveUser] = useState();
+	const [userRepos, setUserRepos] = useState([]);
 
-  return (
-    <>
-      <UserSearch activeUser={activeUser} setActiveUser={setActiveUser} userRepos={userRepos} setUserRepos={setUserRepos} renderCheck={renderCheck} setRenderCheck={setRenderCheck} />
-      <div>
-        <h3>Son Aramalar</h3>
-        <UserList setUser={setActiveUser} setUserRepos={setUserRepos}/>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<UserSearch
+				activeUser={activeUser}
+				setActiveUser={setActiveUser}
+				userRepos={userRepos}
+				setUserRepos={setUserRepos}
+				renderCheck={renderCheck}
+				setRenderCheck={setRenderCheck}
+			/>
+			<div className="bg-transparent">
+				<h3>Son Aramalar</h3>
+				<UserList setUser={setActiveUser} setUserRepos={setUserRepos} />
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
--- Ogrenciler/aytug/Dersler/GroupProjects/02-05(github)/react-styling/src/components/ActiveUser/ActiveUser.jsx
+++ Ogrenciler/aytug/Dersler/GroupProjects/02-05(github)/react-styling/src/components/ActiveUser/ActiveUser.jsx
@@ -6,62 +6,52 @@ const ActiveUser = ({ activeUser, userRepos }) => {
 	//     a.created_at - b.created_at
 	// }));
 	return (
-		<div>
+		<div className="bg-none">
 			<div className="card mb-3">
 				<div className="row g-0">
-					<div className="col-md-4">
+					<div className="col-4">
 						<img src={activeUser.avatar_url} className="img-fluid rounded-start" alt="..." />
 					</div>
-					<div className="col-md-8">
-						<div className="card-body text-start">
+					<div className="col-8">
+						<div className="col-12">
 							<h5 className="card-title">{activeUser.name}</h5>
-							<p className="card-text">
-								{activeUser.bio} | {activeUser.company}
-							</p>
-							<p className="card-text">Repo sayısı : {activeUser.public_repos}</p>
-							<ul className="list-group">
-								Son repolar :<li className="list-group">{userRepos[userRepos.length - 1].name}</li>
-								<li className="list-group">{userRepos[userRepos.length - 2].name}</li>
-							</ul>
-							<button disabled className="btn btn-danger">
-								takip edilen:{activeUser.following}
-							</button>
-							<button disabled className="btn btn-danger">
-								takipçi:{activeUser.followers}
-							</button>
+							<h6 className="card-title">{activeUser.bio}</h6>
 						</div>
-					</div>
-					{/* <div className='col-md-4'>
-                        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                            İncele
-                        </button>
-                    </div> */}
-				</div>
-			</div>
-
-			<div
-				className="modal fade"
-				id="exampleModal"
-				tabIndex="-1"
-				aria-labelledby="exampleModalLabel"
-				aria-hidden="true"
-			>
-				<div className="modal-dialog">
-					<div className="modal-content">
-						<div className="modal-header">
-							<h1 className="modal-title fs-5" id="exampleModalLabel">
-								Modal title
-							</h1>
-							<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-						</div>
-						<div className="modal-body">...</div>
-						<div className="modal-footer">
-							<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
-								Close
-							</button>
-							<button type="button" className="btn btn-primary">
-								Save changes
-							</button>
+						<div className="col-12">
+							<div className="row">
+								<div className="col-6 px-5">
+									<div className="card-body text-start">
+										<p className="card-text mb-2">
+											<i className="bi bi-building"></i>
+											{activeUser.company}
+										</p>
+										<p className="card-text mb-2">
+											<i className="bi bi-pin-map"></i> {activeUser.location}
+										</p>
+										<p>
+											<i className="bi bi-people">
+												{activeUser.followers} takip edilen - {activeUser.following} takipçi
+											</i>
+										</p>
+									</div>
+								</div>
+								<div className="col-6">
+									<p className="card-text mb-2">
+										<i className="bi bi-journal-bookmark-fill"></i> {activeUser.public_repos} repos
+									</p>
+									<ul className="list-group text-start">
+										<li className="list-group">
+											{userRepos[userRepos.length - 1].name} | {userRepos[userRepos.length - 1].created_at.slice(0, 10)}
+										</li>
+										<li className="list-group">
+											{userRepos[userRepos.length - 2].name} | {userRepos[userRepos.length - 2].created_at.slice(0, 10)}
+										</li>
+										<li className="list-group">
+											{userRepos[userRepos.length - 3].name} | {userRepos[userRepos.length - 3].created_at.slice(0, 10)}
+										</li>
+									</ul>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
