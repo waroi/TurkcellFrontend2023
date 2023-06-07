b31edf28a0ff9dece4d5ad79b78e58e3c577693a
İrem Gündoğan <rem1035@hotmail.com>
2023-06-07 14:57:05 +0300

    ..

--- Ogrenciler/IremGundogan/GrupCalısmaları/Ders35-WeatherApp(burcu-fatma)/weatherApp/src/App.jsx
+++ Ogrenciler/IremGundogan/GrupCalısmaları/Ders35-WeatherApp(burcu-fatma)/weatherApp/src/App.jsx
@@ -3,36 +3,48 @@ import Styles from "./App.module.css";
 import Weather from "./components/Weather";
 
 function App() {
-  const [cityWeather, setCityWeather] = useState({});
-  const weatherHandler = async (city) => {
-    await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=082d40dcff70c31b4110a71337d0e26a`
-    ).then((res) => res.json()).then((data) => setCityWeather(data));
+  const [city, setCity] = useState("");
+  const [cityWeather, setCityWeather] = useState(null);
+
+  const weatherHandler = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=082d40dcff70c31b4110a71337d0e26a`
+      );
+      const data = await response.json();
+      setCityWeather(data);
+    } catch (error) {
+      console.error("Hava durumu alınamadı:", error);
+    }
   };
+
   const citySearch = () => {
-    const city = document.querySelector("input").value;
-    console.log(city)
-    weatherHandler(city);
-    console.log(cityWeather.name)
+    weatherHandler();
+  };
+
+  const handleInputChange = (e) => {
+    setCity(e.target.value);
   };
 
   return (
-    <>
-      <div className={Styles.container}>
-        <div className={Styles.app}>
-          <div>
-            <h2>Weather</h2>
-            <input placeholder="Search for a city"></input>
-            <button onClick={citySearch}>ara</button>
-          </div>
-          <div>
-            <div className={Styles.city}></div>
-            <div className={Styles.country}></div>
-          </div>
+    <div className={Styles.container}>
+      <div className={Styles.app}>
+        <div>
+          <h2>Weather</h2>
+          <input
+            placeholder="Search for a city"
+            value={city}
+            onChange={handleInputChange}
+          ></input>
+          <button onClick={citySearch}>ara</button>
         </div>
-        {cityWeather && <Weather data={cityWeather}/> }
+        <div>
+          <div className={Styles.city}></div>
+          <div className={Styles.country}></div>
+        </div>
       </div>
-    </>
+      {cityWeather && <Weather data={cityWeather} />}
+    </div>
   );
 }
 
--- Ogrenciler/IremGundogan/GrupCalısmaları/Ders35-WeatherApp(burcu-fatma)/weatherApp/src/components/Weather/index.jsx
+++ Ogrenciler/IremGundogan/GrupCalısmaları/Ders35-WeatherApp(burcu-fatma)/weatherApp/src/components/Weather/index.jsx
@@ -1,19 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const Weather = (cityData) => {
-  const [weather, setWeather] = useState({});
+const Weather = ({ data }) => {
+  const [weather, setWeather] = useState(null);
 
-  const weatherHandler = async (city) => {
-    await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=082d40dcff70c31b4110a71337d0e26a`
-    )
-      .then((res) => res.json())
-      .then((data) => setWeather(data));
-  };
+  useEffect(() => {
+    const weatherHandler = async () => {
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=082d40dcff70c31b4110a71337d0e26a`
+        );
+        const weatherData = await response.json();
+        setWeather(weatherData);
+      } catch (error) {
+        console.error("Hava durumu alınamadı:", error);
+      }
+    };
 
-  weatherHandler(cityData.data);
+    weatherHandler();
+  }, [data]);
 
-  return <div>{weather}</div>;
+  if (!weather) {
+    return <div>Loading...</div>;
+  }
+
+  // Veriyi her gün için ilk tahmin olacak şekilde filtreleyelim.
+  let day = new Date().getDate();
+  let filteredData = weather.list.filter((item) => {
+    let itemDay = new Date(item.dt_txt).getDate();
+    if (itemDay !== day) {
+      day = itemDay;
+      return true;
+    }
+    return false;
+  });
+
+  return (
+    <div>
+      <h1>{weather.city.name}</h1>
+      {filteredData.map((item, index) => (
+        <div key={index}>
+          <h2>{new Date(item.dt_txt).toLocaleDateString()}</h2>
+          <p>Temperature: {item.main.temp}</p>
+          <p>Condition: {item.weather[0].description}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Weather;
