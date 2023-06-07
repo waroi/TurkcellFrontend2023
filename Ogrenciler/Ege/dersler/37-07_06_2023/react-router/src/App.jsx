1df69398f8302a8f6271af43fbc4b8bcec7af7b8
Ege <karaege98@gmail.com>
2023-06-07 21:42:24 +0300

    lesson37-reactrouter intro

---
+++ Ogrenciler/Ege/dersler/37-07_06_2023/react-router/src/App.jsx
@@ -0,0 +1,12 @@
+import Router from "./routes/Router"
+function App() {
+  return (
+    <>
+      <h1>React Router</h1>
+      <Router />
+      <footer>Footer</footer>
+    </>
+  )
+}
+
+export default App
