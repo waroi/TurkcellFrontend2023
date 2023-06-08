ff94b233a9b58f98d95ddf40fa8299fff1aef021
Selahattin Demir <selahattindemir686@gmail.com>
2023-06-08 14:59:47 +0300

    created Ders35

---
+++ Ogrenciler/SelahattinDemir/Dersler/Ders35/router-app/src/views/ParametreViews.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import { useParams } from "react-router-dom"
+
+const ParametreViews = () => {
+  const {id} = useParams();
+  return (
+    <div>ParametreViews</div>
+  )
+}
+
+export default ParametreViews
