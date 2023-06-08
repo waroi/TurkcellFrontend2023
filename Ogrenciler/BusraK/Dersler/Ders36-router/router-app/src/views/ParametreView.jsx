7bfc0d64ef232a8e2fba683b182b77c44812702e
busra4 <bkosnak10@hotmail.com>
2023-06-08 18:59:05 +0300

    added ders36

---
+++ Ogrenciler/BusraK/Dersler/Ders36-router/router-app/src/views/ParametreView.jsx
@@ -0,0 +1,10 @@
+import { useParams } from "react-router-dom";
+
+const ParametreView = () => {
+  const { id } = useParams();
+  return <div>ParametreView</div>;
+};
+//birkaç sayfalık uygulama yap haber sayfası
+//haber sayfasına gitsin
+//navigasyon bar ile linklerle
+export default ParametreView;
