4f45e24216e11120557be77d8001d10106150186
Aytug <yesilyurt.aytug@hotmail.com>
2023-06-09 00:58:15 +0300

    update

--- Ogrenciler/aytug/Dersler/GroupProjects/08-06(haber)/news-project/src/views/HomeView.jsx
+++ Ogrenciler/aytug/Dersler/GroupProjects/08-06(haber)/news-project/src/views/HomeView.jsx
@@ -2,10 +2,9 @@ import { useRef } from "react";
 import { getNews } from "../services/api";
 import { Link } from "react-router-dom";
 
-const HomeView = ({setLatestNews}) => {
-
-const countryCode = useRef();
-const categoryName = useRef();
+const HomeView = ({ setLatestNews }) => {
+	const countryCode = useRef();
+	const categoryName = useRef();
 	const getNewsFromDb = async () => {
 		console.log("countryCode.current.value", countryCode.current.value);
 		console.log("categoryName.current.value", categoryName.current.value);
@@ -20,36 +19,35 @@ const categoryName = useRef();
 			<div className="row">
 				<div className="col-8 d-flex flex-column mx-auto mt-3 align-items-center justify-content-center">
 					<div className="inputArea mt-3">
-					<label htmlFor="countries">Ülke Seçin</label>
-					<select className="form-select mt-2" name="countries" ref={countryCode}>
-						<option value="tr">:flag_tr Türkiye</option>
-						<option value="us">Amerika</option>
-						<option value="ru">Rusya</option>
-						<option value="de">Almanya</option>
-						<option value="gb">Birleşik Krallık</option>
-					</select>
+						<label htmlFor="countries">Ülke Seçin</label>
+						<select className="form-select mt-2" name="countries" ref={countryCode}>
+							<option value="tr">🇹🇷 Türkiye</option>
+							<option value="us">🇺🇸 Amerika</option>
+							<option value="ru">🇷🇺 Rusya</option>
+							<option value="de">🇩🇪 Almanya</option>
+							<option value="gb">🇬🇧 Birleşik Krallık</option>
+						</select>
 					</div>
-				<div className="inputArea mt-3">
-				<label htmlFor="categories">Kategori Seçin</label>
-					<select className="form-select mt-2"  name="categories" ref={categoryName}>
-						<option value="general">Genel</option>
-						<option value="business">İş Dünyası</option>
-						<option value="science">Bilim</option>
-						<option value="technology">Teknoloji</option>
-						<option value="sport">Spor</option>
-					</select>
-				</div>
+					<div className="inputArea mt-3">
+						<label htmlFor="categories">Kategori Seçin</label>
+						<select className="form-select mt-2" name="categories" ref={categoryName}>
+							<option value="general">Genel</option>
+							<option value="business">İş Dünyası</option>
+							<option value="science">Bilim</option>
+							<option value="technology">Teknoloji</option>
+							<option value="sport">Spor</option>
+						</select>
+					</div>
 					{
-					<Link
-						to="/news"
-						className="btn btn-success mt-3 w-100"
-						onClick={() => {
-							getNewsFromDb();
-						}}
-						
-					>
-						Getir
-					</Link>
+						<Link
+							to="/news"
+							className="btn btn-success mt-3 w-100"
+							onClick={() => {
+								getNewsFromDb();
+							}}
+						>
+							Getir
+						</Link>
 					}
 				</div>
 			</div>
