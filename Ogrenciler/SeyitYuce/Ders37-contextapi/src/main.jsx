042a6fc9478ec526f6e953c2b4288d180660769a
Seyit Yuce <111066041+seyityuce@users.noreply.github.com>
2023-06-10 01:10:25 +0300

    theme-changing

--- Ogrenciler/SeyitYuce/Ders37-contextapi/src/main.jsx
+++ Ogrenciler/SeyitYuce/Ders37-contextapi/src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThemeContext } from "./context/ThemeContext";
+import { ThemeProvider } from "./context/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeContext>
+    <ThemeProvider>
       <App />
-    </ThemeContext>
+    </ThemeProvider>
   </React.StrictMode>
 );
