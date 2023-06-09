af1ee3636497c28c5efa27845babcfc106d83d13
07kagan07 <kagancoskun32@gmail.com>
2023-06-09 17:31:59 +0300

    curr

--- Ogrenciler/KaganCoskun/GrupCalismalari/Ders36-NewsPaper/src/services/requets.js
+++ Ogrenciler/KaganCoskun/GrupCalismalari/Ders36-NewsPaper/src/services/requets.js
@@ -1,5 +1,5 @@
 export async function getNews(category,page,country){
-    const data = await fetch(`${import.meta.env.VITE_API_URI}?country=${country}&tag=${category}&paging=${page}`,{
+    const data = await fetch(`${import.meta.env.VITE_NEWS_API_URI}?country=${country}&tag=${category}&paging=${page}`,{
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -11,9 +11,22 @@ export async function getNews(category,page,country){
 
 }
 
-
 export async function getNewsDetail(id,page,category,country){
     const newsPage =await getNews(category,page,country)
     const news = newsPage?.result?.find((news)=>news.key===id)
     return news
+}
+
+export async function getAllCurrency(){
+    const data = await fetch(`${import.meta.env.VITE_CURRENCY_API_URI}/allCurrency `,{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'authorization': `apikey ${import.meta.env.VITE_API_KEY}`
+        }
+    })
+    const currency = await data.json()
+    console.log(currency)
+    return currency
+
 }
