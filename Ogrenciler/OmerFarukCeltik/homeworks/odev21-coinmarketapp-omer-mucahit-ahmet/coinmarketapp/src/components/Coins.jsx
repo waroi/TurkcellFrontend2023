84ef55272945f48d80e81154e421e418e6736276
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-06-10 22:36:05 +0300

    table added
    Co-Authored-By: Mücahit <66551084+mchtkuk@users.noreply.github.com>
    Co-Authored-By: Ahmet Mert Tutal <a.tutal1997@gmail.com>

--- Ogrenciler/OmerFarukCeltik/homeworks/odev21-coinmarketapp-omer-mucahit-ahmet/coinmarketapp/src/components/Coins.jsx
+++ Ogrenciler/OmerFarukCeltik/homeworks/odev21-coinmarketapp-omer-mucahit-ahmet/coinmarketapp/src/components/Coins.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { useFetch } from '../context/FetchContext'
+import { Link } from 'react-router-dom';
 
 const Coins = () => {
+  const { datas } = useFetch();
   return (
-    <div>Coins</div>
+    <div>
+      <table>
+        <thead>
+          <th>#</th>
+          <th>logo</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th>1h %</th>
+          <th>1d %</th>
+          <th>1w %</th>
+          <th>Market Cap</th>
+          <th>Volume 24h</th>
+          <th>Total Supply</th>
+        </thead>
+        <tbody>
+        {datas.map((item,i) => (
+          <tr>
+            {/* <Link key={item.id} to={`/coinpage/${item.id}`}> */}
+            <td>{i}</td>
+            <td><img className='table-img' src={item.icon} alt="" /></td>
+            <td>{item.name}</td>
+            <td>{item.price.toString().slice(0,7)}$</td>
+            <td>{item.priceChange1d} %</td>
+            <td>{item.priceChange1h} %</td>
+            <td>{item.priceChange1w} %</td>
+            <td>{item.marketCap.toLocaleString().slice(0,6) }M $</td>
+            <td>{(item.volume/1000000).toLocaleString()} M {item.symbol}</td>
+            <td>{item.totalSupply.toLocaleString()} {item.symbol}</td>
+          {/* </Link> */}
+          </tr>
+        ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
