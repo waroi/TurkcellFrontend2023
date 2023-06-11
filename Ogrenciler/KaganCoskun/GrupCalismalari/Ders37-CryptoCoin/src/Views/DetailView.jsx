e3bf16836a2ca29c6fd88d5ddbc4d0cbcbfd417a
07kagan07 <kagancoskun32@gmail.com>
2023-06-11 23:46:08 +0300

    update

--- Ogrenciler/KaganCoskun/GrupCalismalari/Ders37-CryptoCoin/src/Views/DetailView.jsx
+++ Ogrenciler/KaganCoskun/GrupCalismalari/Ders37-CryptoCoin/src/Views/DetailView.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useCoinList } from "../context/CoinContext";
-import { Top, Bottom, SpanItem, DetailWrapper, Table, TDimension, TRow, Rank,Image,Paragraph,Span,Header2} from "./detailStyled";
+import { Top, Bottom, SpanItem, DetailWrapper, Table, Rank,Image,Paragraph,Span,Header2} from "./detailStyled";
 import { useCurrency } from "../context/CurrencyContext";
 import { useTheme } from "../context/ThemeContext";
 import TableItem from "../components/TableItem/TableItem";
@@ -32,18 +32,18 @@ const DetailView = () => {
         </Paragraph>
       </Top>
       <Bottom>
-        <Header2 theme={theme}>{currency.toUpperCase() + " " + newCoin?.current_price}</Header2>
+        <Header2 theme={theme}>{currency.toUpperCase() + " " + newCoin?.current_price.toLocaleString()}</Header2>
         <SpanItem price={newCoin?.price_change_24h}>
           {newCoin?.price_change_percentage_24h.toFixed(1)}%
         </SpanItem>
       </Bottom>
 <Table>
   <tbody >
-    <TableItem text="Piyasa Degeri" value={newCoin?.market_cap}/>
-    <TableItem text="Total Supply" value={newCoin?.total_supply}/>
-    <TableItem text="Total volume" value={newCoin?.total_volume}/>
-    <TableItem text="Circulating Supply" value={newCoin?.circulating_supply}/>
-    <TableItem text="Fully Diluted Valuation" value={newCoin?.fully_diluted_valuation}/>
+    <TableItem text="Piyasa Degeri" value={newCoin?.market_cap.toLocaleString()}/>
+    <TableItem text="Total Supply" value={newCoin?.total_supply.toLocaleString()}/>
+    <TableItem text="Total volume" value={newCoin?.total_volume.toLocaleString()}/>
+    <TableItem text="Circulating Supply" value={newCoin?.circulating_supply.toLocaleString()}/>
+    <TableItem text="Fully Diluted Valuation" value={newCoin?.fully_diluted_valuation.toLocaleString()}/>
   </tbody>
 </Table>
     </DetailWrapper>
