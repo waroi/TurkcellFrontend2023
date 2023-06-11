15cc2f8e9866a83605f5668f3376c903bf761316
BuseTurkmen <buse.turkmen2000@gmail.com>
2023-06-11 20:20:08 +0300

    news cards
    Co-Authored-By: Seyit Yuce <111066041+seyityuce@users.noreply.github.com>
    Co-Authored-By: M.Furkan Uygur <76523870+mfurkanuygur@users.noreply.github.com>

--- Ogrenciler/SeyitYuce/Ders37-contextapi/hw-coinapp/vite-project/src/Components/News/News.jsx
+++ Ogrenciler/SeyitYuce/Ders37-contextapi/hw-coinapp/vite-project/src/Components/News/News.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getNews } from "../../service/api";
+import "./News.css";
 
 const News = () => {
   const [news, setNews] = useState([]);
@@ -7,9 +8,9 @@ const News = () => {
   useEffect(() => {
     getNews().then((data) => {
       if (data?.articles.length == 0) {
-        setLoading(true);
+        setLoading(false);
       } else {
-        // setNews(data);
+        setNews(data)
         setLoading(false);
       }
     });
@@ -19,20 +20,26 @@ const News = () => {
   }
   console.log(news.articles.length);
   return (
-    <div className="row">
+    <div className="container">
+      <div className="row my-5">
       {news?.articles.map((item, index) => (
-        <div className="col-4 card" key={index}>
+        <div className="col-3 my-3" key={index}>
+          <div className="card">
           <img src="https://picsum.photos/id/180/500/400" alt="" />
-          <span>{item.subject}</span>
-          <span>{item.source}</span>
-          <h2>{item.title}</h2>
+          <div className="d-flex justify-content-between my-2">
+              <span>{item.subject}</span>
+              <span>{item.source}</span>
+          </div>
+          <h2 className="newstitles">{item.title}</h2>
           <p>{item.description}</p>
           <a href={item.url} target="_blank">
             For more
-            <i className="bi bi-box-arrow-up-right"></i>
+              <i className="bi bi-box-arrow-up-right"></i>
           </a>
+          </div>
         </div>
       ))}
+      </div>
     </div>
   );
 };
