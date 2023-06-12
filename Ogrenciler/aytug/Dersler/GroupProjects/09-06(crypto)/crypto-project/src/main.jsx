1c5e0be410d4c15560a0fd44bc9e9f1c9a998964
Selahattin Demir <selahattindemir686@gmail.com>
2023-06-12 15:10:42 +0300

    added LanguageContext

--- Ogrenciler/aytug/Dersler/GroupProjects/09-06(crypto)/crypto-project/src/main.jsx
+++ Ogrenciler/aytug/Dersler/GroupProjects/09-06(crypto)/crypto-project/src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "./context/ThemeContext";
+import { LanguageProvider } from "./context/LanguageContext";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<ThemeProvider>
-				<App />
-			</ThemeProvider>
-		</BrowserRouter>
-	</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <LanguageProvider>
+          <App />
+        </LanguageProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
