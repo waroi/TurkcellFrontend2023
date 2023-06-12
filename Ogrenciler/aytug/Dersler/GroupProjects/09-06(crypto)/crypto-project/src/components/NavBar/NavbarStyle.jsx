369635bf4f3dfb6bdc7c01cc786b4ca3b8b21896
Aytug <yesilyurt.aytug@hotmail.com>
2023-06-12 14:29:26 +0300

    updated navbar

--- Ogrenciler/aytug/Dersler/GroupProjects/09-06(crypto)/crypto-project/src/components/NavBar/NavbarStyle.jsx
+++ Ogrenciler/aytug/Dersler/GroupProjects/09-06(crypto)/crypto-project/src/components/NavBar/NavbarStyle.jsx
@@ -1,17 +1,18 @@
 import styled from "styled-components";
 
 export const NavBarArea = styled.div`
-  display: flex;
-  justify-content: space-between;
-  background-color: #0d1421;
-  padding: 1rem 1rem;
-  * {
-    margin-bottom: 0;
-  }
+	display: flex;
+	justify-content: space-between;
+	background-color: #0d1421;
+	padding: 1rem 1rem;
+	border-bottom: 1px solid rgb(166, 176, 195);
+	* {
+		margin-bottom: 0;
+	}
 `;
 
 export const HeaderArea = styled(NavBarArea)``;
 
 export const NavBarSpan = styled.span`
-  color: #4688ff;
+	color: #4688ff;
 `;
