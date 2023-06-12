c63a7889db5998cd11dcdce52394b8c160937fa4
Selahattin Demir <selahattindemir686@gmail.com>
2023-06-12 16:53:10 +0300

    updated link

--- Ogrenciler/aytug/Dersler/GroupProjects/09-06(crypto)/crypto-project/src/views/NotFound.jsx
+++ Ogrenciler/aytug/Dersler/GroupProjects/09-06(crypto)/crypto-project/src/views/NotFound.jsx
@@ -1,9 +1,12 @@
 const NotFound = () => {
-	return (
-		<div>
-			<img src="../assets/404.png" alt="404 not found" />
-		</div>
-	);
+  return (
+    <div>
+      <img
+        src="https://storage.googleapis.com/www-paredro-com/uploads/2017/07/404-not-found-01.jpg"
+        alt="404 not found"
+      />
+    </div>
+  );
 };
 
 export default NotFound;
