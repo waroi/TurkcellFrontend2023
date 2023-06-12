ceeafecb2b069e493960808def79d00e60be1126
Burakhan Katdar <burakhankatdarr@gmail.com>
2023-06-12 18:01:30 +0300

    Trend Coinler entegre edildi. Tasarımı yapıldı

---
+++ Ogrenciler/KaganCoskun/GrupCalismalari/Ders37-CryptoCoin/src/components/Trendings/trendingStyled.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const TrendCoins = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+`;
+
+export const TrendCoinImage = styled.img`
+  width: 40px;
+  border-radius: 50%;
+`;
+
+export const TrendCoinsItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 15px;
+  font-size: 18px;
+`;
