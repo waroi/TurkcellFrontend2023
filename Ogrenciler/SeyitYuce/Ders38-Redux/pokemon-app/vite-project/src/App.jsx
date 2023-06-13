d64f9898ecdc60ed034b2da44abb3127dd8a6aea
Seyit Yuce <111066041+seyityuce@users.noreply.github.com>
2023-06-13 23:42:06 +0300

    pokemon-hw

---
+++ Ogrenciler/SeyitYuce/Ders38-Redux/pokemon-app/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Router from "./router/Router";
+
+function App() {
+  return (
+    <>
+      <Router />
+    </>
+  );
+}
+
+export default App;
