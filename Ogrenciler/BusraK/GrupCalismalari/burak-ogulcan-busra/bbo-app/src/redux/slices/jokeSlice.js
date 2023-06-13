ccd6db96c9a5bc024738131a22c590d8c8b14ae3
busra4 <bkosnak10@hotmail.com>
2023-06-13 22:32:24 +0300

    added

---
+++ Ogrenciler/BusraK/GrupCalismalari/burak-ogulcan-busra/bbo-app/src/redux/slices/jokeSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+import ChuckJokes from "../../services/api";
+export const jokeSlice = createSlice({
+  name: "joke",
+  initialState: {
+    joke: "",
+    loading: false,
+    error: "",
+    jokes: [],
+    categories: [],
+  },
+  reducers: {
+    getRandomJoke: (state, action) => {
+      state.joke = action.payload;
+    },
+    setCategories: (state, action) => {
+      state.categories = action.payload;
+    },
+    setJoke: (state, action) => {
+      state.joke = action.payload;
+    },
+    setJokes: (state, action) => {
+      state.jokes = action.payload.map((joke) => joke.value);
+    },
+  },
+});
+
+export const { getRandomJoke, setCategories, setJoke, setJokes } =
+  jokeSlice.actions;
+export default jokeSlice.reducer;
