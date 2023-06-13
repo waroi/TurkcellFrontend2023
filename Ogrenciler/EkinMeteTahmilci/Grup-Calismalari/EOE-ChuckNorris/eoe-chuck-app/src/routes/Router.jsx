57c507dd5f654e4c9a331071bd15b3a92712d6dd
Ekin Mete Tahmilci <emtahmilci@gmail.com>
2023-06-13 22:13:19 +0300

    update

--- Ogrenciler/EkinMeteTahmilci/Grup-Calismalari/EOE-ChuckNorris/eoe-chuck-app/src/routes/Router.jsx
+++ Ogrenciler/EkinMeteTahmilci/Grup-Calismalari/EOE-ChuckNorris/eoe-chuck-app/src/routes/Router.jsx
@@ -4,13 +4,16 @@ import MainView from '../views/MainView'
 import RandomJoke from '../components/RandomJoke/RandomJoke'
 
 const Router = () => {
-    const router = useRoutes([
+    const routes = useRoutes([
         {
-            path: "/", element: <MainView/>, children: [{index:true, element: <Navigate to="random"/>},{path: "random", element:<RandomJoke/>}]
+            path: "/", element: <MainView/>, children: [
+                {index:true, element: <Navigate to="random"/>},
+                {path: "random", element:<RandomJoke/>}
+            ]
 
         }
     ])
-
+    return routes;
 }
 
 export default Router
