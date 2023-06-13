4f8d4182c4a8ca68b64bfe08f96031ccf950bbf2
osmancankucuk <76538726+osmancankucuk@users.noreply.github.com>
2023-06-14 01:38:38 +0300

    search-style

--- Ogrenciler/EkinMeteTahmilci/Grup-Calismalari/EOE-ChuckNorris/eoe-chuck-app/src/components/SearchJoke/SearchJoke.jsx
+++ Ogrenciler/EkinMeteTahmilci/Grup-Calismalari/EOE-ChuckNorris/eoe-chuck-app/src/components/SearchJoke/SearchJoke.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { JokeCard } from "./SearchJokeStyle";
 
 const SearchJoke = () => {
   const jokes = useSelector((state) => state.searchJoke);
@@ -6,7 +7,19 @@ const SearchJoke = () => {
 
   return (
     <div>
-      {jokes.length > 0 && jokes.slice(0, 20).map((item) => <div key={item.id}>{item.value} </div>)}
+      <div className="row">
+        {jokes.length > 0 ? (
+          jokes.slice(0, 20).map((item) => (
+            <div key={item.id} className="col-md-4 mb-3">
+              <JokeCard>
+                <div>{item.value} </div>
+              </JokeCard>
+            </div>
+          ))
+        ) : (
+          <div>Uygun bir deger giriniz</div>
+        )}
+      </div>
     </div>
   );
 };
--- Ogrenciler/EkinMeteTahmilci/Grup-Calismalari/EOE-ChuckNorris/eoe-chuck-app/src/components/SearchJoke/SearchJokeStyle.js
+++ Ogrenciler/EkinMeteTahmilci/Grup-Calismalari/EOE-ChuckNorris/eoe-chuck-app/src/components/SearchJoke/SearchJokeStyle.js
@@ -4,5 +4,7 @@ export const JokeCard = styled.div`
   width: 100%;
   border: 1px solid gray;
   border-radius: 10px;
-  height: 100%;
+  height: 300px;
+  padding: 1em;
+  overflow: scroll;
 `;
