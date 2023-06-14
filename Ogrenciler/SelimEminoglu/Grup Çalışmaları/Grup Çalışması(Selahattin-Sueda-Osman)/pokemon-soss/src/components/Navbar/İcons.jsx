f5892df4c4f87abeb827d34207b99be36d0f9e7e
Sueda Çiçekli <suedacicekli@gmail.com>
2023-06-14 18:23:29 +0300

    deneme

--- Ogrenciler/SelimEminoglu/Grup Çalışmaları/Grup Çalışması(Selahattin-Sueda-Osman)/pokemon-soss/src/components/Navbar/İcons.jsx
+++ Ogrenciler/SelimEminoglu/Grup Çalışmaları/Grup Çalışması(Selahattin-Sueda-Osman)/pokemon-soss/src/components/Navbar/İcons.jsx
@@ -1,5 +1,11 @@
 import { Navbarİmg, İconLi, NavbarA, NavbarSpan } from "./styleNavbar";
 import { AiFillHome } from "react-icons/ai";
+import { MdCatchingPokemon, MdOutlineGamepad } from "react-icons/md";
+import { RxCrumpledPaper } from "react-icons/rx";
+import { SiPokemon } from "react-icons/si"
+import { GiTrophyCup } from "react-icons/gi"
+import { BsNewspaper } from "react-icons/bs"
+
 import { Link } from "react-router-dom";
 
 function İcons() {
@@ -16,37 +22,37 @@ function İcons() {
         </İconLi>
         <İconLi hovercolor="#e3350d">
           <NavbarA href="#">
-            <Navbarİmg src="./src/assets/icons/pikachu.png" alt="logo" />
+            <MdCatchingPokemon size={32} style={{ color: "#898989" }} />
             <NavbarSpan>Pokedex</NavbarSpan>
           </NavbarA>
         </İconLi>
         <İconLi hovercolor="#ee6b2f">
           <NavbarA href="#">
-            <Navbarİmg src="./src/assets/icons/pikachu.png" alt="logo" />
+            <MdOutlineGamepad size={32} style={{ color: "#898989" }} />
             <NavbarSpan>Video Games & Apps</NavbarSpan>
           </NavbarA>
         </İconLi>
         <İconLi hovercolor="#e6bc2f">
           <NavbarA href="#">
-            <Navbarİmg src="./src/assets/icons/pikachu.png" alt="logo" />
+            <RxCrumpledPaper size={32} style={{ color: "#898989" }} />
             <NavbarSpan>Trading Card Game</NavbarSpan>
           </NavbarA>
         </İconLi>
         <İconLi hovercolor="#4dad5b">
           <NavbarA href="#">
-            <Navbarİmg src="./src/assets/icons/pikachu.png" alt="logo" />
+            <SiPokemon size={42} style={{ color: "#898989" }} />
             <NavbarSpan>Pokemon TV</NavbarSpan>
           </NavbarA>
         </İconLi>
         <İconLi hovercolor="#30a7d7">
           <NavbarA href="#">
-            <Navbarİmg src="./src/assets/icons/pikachu.png" alt="logo" />
+            <GiTrophyCup size={32} style={{ color: "#898989" }} />
             <NavbarSpan>Play! Pokemon Events</NavbarSpan>
           </NavbarA>
         </İconLi>
         <İconLi hovercolor="#1b53ba">
           <NavbarA href="#">
-            <Navbarİmg src="./src/assets/icons/pikachu.png" alt="logo" />
+            <BsNewspaper size={32} style={{ color: "#898989" }} />
             <NavbarSpan>News</NavbarSpan>
           </NavbarA>
         </İconLi>
