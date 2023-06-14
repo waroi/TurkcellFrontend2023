032d87c11dd508c863a178f220e5d8741428ed13
Seyit Yuce <111066041+seyityuce@users.noreply.github.com>
2023-06-14 21:13:43 +0300

    nextjs

---
+++ Ogrenciler/SeyitYuce/ders39-Next/first-next/src/app/about/layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const aboutLayout = ({ children }) => {
+  return (
+    <div>
+      <h2>about sayfasının layout bölümü</h2>
+      {children}
+    </div>
+  );
+};
+
+export default layout;
