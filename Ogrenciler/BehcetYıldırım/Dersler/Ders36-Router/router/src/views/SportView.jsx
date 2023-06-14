904bd4ecb6059a94f15c2ae35eb14d4dbdd17228
Sait Behçet Yıldırım <saitbehcet@gmail.com>
2023-06-14 18:39:44 +0300

    Ders37

--- Ogrenciler/BehcetYıldırım/Dersler/Ders36-Router/router/src/views/SportView.jsx
+++ Ogrenciler/BehcetYıldırım/Dersler/Ders36-Router/router/src/views/SportView.jsx
@@ -3,11 +3,11 @@ import { Outlet, useParams } from "react-router-dom";
 const SportView = () => {
   const { name } = useParams();
   return (
-    <h3>
+    <h1>
       Sport View
       <Outlet />
       {name}
-    </h3>
+    </h1>
   );
 };
 
