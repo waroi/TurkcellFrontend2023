5d917e861ab2d2aadf719b37767015b5ded723fe
Sueda Çiçekli <suedacicekli@gmail.com>
2023-06-16 19:28:45 +0300

    final...

---
+++ Ogrenciler/SuedaCicekli/TeamWork/formik/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import GeneralForm from './components/GeneralForm.jsx'
+
+function App() {
+
+
+  return (
+    <>
+      <GeneralForm />
+    </>
+  )
+}
+
+export default App
