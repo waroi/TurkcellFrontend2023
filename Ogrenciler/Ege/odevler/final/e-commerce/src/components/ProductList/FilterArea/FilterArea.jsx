864b796f17ff386db804d26a3af5cbf53530620f
Ege <karaege98@gmail.com>
2023-06-23 19:18:35 +0300

    filter by category done

--- Ogrenciler/Ege/odevler/final/e-commerce/src/components/ProductList/FilterArea/FilterArea.jsx
+++ Ogrenciler/Ege/odevler/final/e-commerce/src/components/ProductList/FilterArea/FilterArea.jsx
@@ -1,10 +1,41 @@
-const FilterArea = () => {
+import { useEffect, useState } from "react"
+import PropTypes from "prop-types"
+const FilterArea = ({ products, checkedCategories, setCheckedCategories }) => {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        setCategories(Array.from(new Set(products.map(product => product.category.toLowerCase()))))
+    }, [products])
+
+    const handleChange = (e) => {
+        if (e.target.checked) setCheckedCategories([...checkedCategories, e.target.value])
+        else setCheckedCategories([...checkedCategories].filter(cat => cat != e.target.value))
+    }
+
+
     return (
         <div>
             <h1>Filter</h1>
-            <p>Filter Area</p>
+            <div className="checkboxes">
+                {
+                    categories.map(category => (
+                        <div key={category}>
+                            <input type="checkbox" id={category} name={category} value={category} onChange={handleChange}
+                            />
+                            <label htmlFor={category}>{category}</label><br></br>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
 
+FilterArea.propTypes = {
+    products: PropTypes.array.isRequired,
+    checkedCategories: PropTypes.array.isRequired,
+    setCheckedCategories: PropTypes.func.isRequired
+}
+
 export default FilterArea
+
--- Ogrenciler/Ege/odevler/final/e-commerce/src/components/ProductList/ProductList.jsx
+++ Ogrenciler/Ege/odevler/final/e-commerce/src/components/ProductList/ProductList.jsx
@@ -8,31 +8,55 @@ import { Link } from "react-router-dom";
 const ProductList = () => {
 
     const [products, setProducts] = useState([]);
+    const [checkedCategories, setCheckedCategories] = useState([]);
 
     useEffect(() => {
         axios.get('http://localhost:3000/products')
-            .then(response => setProducts(response.data))
+            .then(response => setProducts(response.data.map(el => ({ ...el, isFiltered: true, isSearched: true }))))
             .catch(err => console.log('Error fetching product data:', err))
 
     }, []);
+
+
+    const handleCategories = () => {
+        if (checkedCategories.length > 0) {
+            const edited = [...products]
+            edited.map(product => checkedCategories.includes(product.category.toLowerCase()) ? product.isFiltered = true : product.isFiltered = false)
+            setProducts(edited)
+            console.log("I am here")
+        }
+        else {
+            const edited = [...products]
+            edited.map(product => product.isFiltered = true)
+            setProducts(edited)
+        }
+    }
+
+    useEffect(() => { handleCategories() }, [checkedCategories])
+
+
     return (
         <div className="container">
             <div className="row">
                 <div className="col-lg-3">
-                    <FilterArea />
+                    <FilterArea products={products} checkedCategories={checkedCategories} setCheckedCategories={setCheckedCategories} />
+                    <div className="selectedCategories">
+                        {
+                            checkedCategories.map((selected, i) => (<p key={i}>{selected}</p>))
+                        }
+                    </div>
                 </div>
                 <div className="col-lg-9">
                     <SortArea />
                     <div className="container">
                         <div className="row">
                             {
-                                products?.map(product => (
+                                products?.map(product => product.isFiltered && (
                                     <Link key={product.id} className="toPageLink col-lg-4" to={`/products/${product.id}`}>
                                         <ProductCard product={product}>
 
                                         </ProductCard>
                                     </Link>
-
                                 ))
                             }
                         </div>
