e50d51312e3daa0bd049b72491887aea2fb05de6
Selahattin Demir <selahattindemir686@gmail.com>
2023-06-24 22:13:03 +0300

    created ProductsSection folder

---
+++ Ogrenciler/SelahattinDemir/Odevler/Odev-5/final-project/src/components/ProductsSection/ProductsSection.jsx
@@ -0,0 +1,41 @@
+import Proptypes from "prop-types";
+import Card from "../Card/Card";
+import { TopTitle, BottomTitle, Button } from "../NewsSection/NewsSectionStyle";
+
+function ProductsSection({ products }) {
+  const shuffle = (array) => {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
+  };
+
+  const randomProducts = shuffle(products).slice(0, 8);
+  return (
+    <div className="row">
+      <div className="d-flex justify-content-between align-items-center my-3">
+        <div>
+          <TopTitle>Hard to choose right products for your pets?</TopTitle>
+          <BottomTitle>Our Products</BottomTitle>
+        </div>
+        <Button>
+          View more <i className="bi bi-chevron-right"></i>
+        </Button>
+      </div>
+      {randomProducts.map((product) => (
+        <Card key={product.id} data={product} />
+      ))}
+    </div>
+  );
+}
+
+ProductsSection.propTypes = {
+  products: Proptypes.array.isRequired,
+};
+
+export default ProductsSection;
