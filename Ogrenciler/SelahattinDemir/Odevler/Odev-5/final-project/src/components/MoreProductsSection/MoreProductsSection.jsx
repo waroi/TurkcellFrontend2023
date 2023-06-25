33b5d2e7e9fea1ca5ce50ebd645b5999a4b5117b
Selahattin Demir <selahattindemir686@gmail.com>
2023-06-26 00:09:13 +0300

    created MoreProductsSection

---
+++ Ogrenciler/SelahattinDemir/Odevler/Odev-5/final-project/src/components/MoreProductsSection/MoreProductsSection.jsx
@@ -0,0 +1,46 @@
+import Proptypes from "prop-types";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchProducts } from "../../redux/slice/productsSlice";
+import { TopTitle, BottomTitle } from "../NewsSection/NewsSectionStyle";
+import Card from "../Card/Card";
+
+function MoreProductsSection({ id }) {
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  const getRandomProducts = () => {
+    const filteredProducts = products.filter((product) => product.id !== id);
+    const randomProducts = [];
+
+    while (randomProducts.length < 8 && filteredProducts.length > 0) {
+      const randomIndex = Math.floor(Math.random() * filteredProducts.length);
+      randomProducts.push(filteredProducts[randomIndex]);
+      filteredProducts.splice(randomIndex, 1);
+    }
+
+    return randomProducts;
+  };
+
+  const randomProducts = getRandomProducts();
+
+  return (
+    <div className="row">
+      <TopTitle>Whats new?</TopTitle>
+      <BottomTitle>See more products</BottomTitle>
+      {randomProducts.map((product) => (
+        <Card key={product.id} data={product} />
+      ))}
+    </div>
+  );
+}
+
+MoreProductsSection.propTypes = {
+  id: Proptypes.string.isRequired,
+};
+
+export default MoreProductsSection;
