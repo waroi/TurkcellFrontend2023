93c82b161c4dd2d0d20cdf9ae81113c76f808df7
Selahattin Demir <selahattindemir686@gmail.com>
2023-06-25 23:49:58 +0300

    created RegisterView file

--- Ogrenciler/SelahattinDemir/Odevler/Odev-5/final-project/src/views/RegisterView.jsx
+++ Ogrenciler/SelahattinDemir/Odevler/Odev-5/final-project/src/views/RegisterView.jsx
@@ -1,16 +1,68 @@
 import { useFormik } from "formik";
 import { registerSchema } from "../schemas";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import Request from "../utils/Request";
 
 function RegisterView() {
-  const onSubmit = async (values, actions) => {
-    console.log(values);
-    console.log(actions);
+  const request = new Request("http://localhost:3004/users");
 
-    await new Promise((resolve) => {
-      setTimeout(resolve, 1000);
-    });
-    actions.resetForm();
+  const navigate = useNavigate();
+
+  const checkExistingUser = async (email, username) => {
+    // Mevcut kullanıcıları almak için bir istek gönderin veya veritabanına sorgu yapın
+    // Örnek olarak, mevcut kullanıcıları almak için bir API çağrısı yapabilirsiniz
+    const response = await request.get();
+
+    // Email ve kullanıcı adı kontrolü yapın
+    const existingUser = response.find(
+      (user) => user.email === email || user.username === username
+    );
+
+    return existingUser;
+  };
+
+  const onSubmit = async (values, actions) => {
+    try {
+      // Mevcut kullanıcıları kontrol edin
+      const existingUser = await checkExistingUser(
+        values.email,
+        values.username
+      );
+
+      if (existingUser) {
+        toast.error(
+          "Email or username already exists. Please choose a different one."
+        );
+        actions.setSubmitting(false);
+        actions.resetForm();
+        return;
+      }
+
+      // Kayıt işlemini gerçekleştirin
+      const response = await request.post({
+        ...values,
+        login: true,
+        role: "user",
+        status: 201,
+        carts: [],
+      });
+
+      console.log(response);
+
+      if (response.status === 201) {
+        toast.success("Registration successful. You are logged in.");
+        actions.resetForm();
+      } else {
+        toast.error("An error occurred. Please try again.");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("An error occurred. Please try again.");
+    }
+
+    actions.setSubmitting(false);
+    navigate("/");
   };
 
   const { values, errors, isSubmitting, handleChange, handleSubmit } =
@@ -27,7 +79,7 @@ function RegisterView() {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form className="register-form" onSubmit={handleSubmit}>
         <div className="inputDiv">
           <label>Email</label>
           <input
@@ -35,55 +87,57 @@ function RegisterView() {
             value={values.email}
             onChange={handleChange}
             id="email"
-            placeholder="Mail adresinizi giriniz"
+            placeholder="Enter your email"
             className={errors.email ? "input-error" : ""}
           />
           {errors.email && <p className="error">{errors.email}</p>}
         </div>
         <div className="inputDiv">
-          <label>Kullanıcı Adı</label>
+          <label>Username</label>
           <input
             type="string"
             value={values.username}
             onChange={handleChange}
             id="username"
-            placeholder="Kullanıcı adını giriniz"
+            placeholder="Enter your username"
             className={errors.username ? "input-error" : ""}
           />
           {errors.username && <p className="error">{errors.username}</p>}
         </div>
         <div className="inputDiv">
-          <label>Şifre</label>
+          <label>Password</label>
           <input
             type="password"
             value={values.password}
             onChange={handleChange}
             id="password"
-            placeholder="Şifrenizi giriniz"
+            placeholder="Enter your password"
             className={errors.password ? "input-error" : ""}
           />
           {errors.password && <p className="error">{errors.password}</p>}
         </div>
         <div className="inputDiv">
-          <label>Şifre Tekrar</label>
+          <label>Password Repeat</label>
           <input
             type="password"
             value={values.confirmPassword}
             onChange={handleChange}
             id="confirmPassword"
-            placeholder="Şifrenizi tekrar giriniz"
+            placeholder="Enter your password again"
             className={errors.confirmPassword ? "input-error" : ""}
           />
           {errors.confirmPassword && (
             <p className="error">{errors.confirmPassword}</p>
           )}
         </div>
-        <button disabled={isSubmitting} type="submit">
-          Kayıt Ol
-        </button>
-        <Link className="formLink" to="/login">
-          Giriş Yap
-        </Link>
+        <div className="d-flex align-items-center justify-content-evenly">
+          <button className="formLink" disabled={isSubmitting} type="submit">
+            Signup
+          </button>
+          <Link className="formLink" to="/login">
+            Login
+          </Link>
+        </div>
       </form>
     </div>
   );
