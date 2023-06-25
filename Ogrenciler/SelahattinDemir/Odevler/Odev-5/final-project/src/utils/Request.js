0df52681945cfc64bd29ce674f831e6408c1eac2
Selahattin Demir <selahattindemir686@gmail.com>
2023-06-26 00:08:28 +0300

    deleted comment lines

--- Ogrenciler/SelahattinDemir/Odevler/Odev-5/final-project/src/utils/Request.js
+++ Ogrenciler/SelahattinDemir/Odevler/Odev-5/final-project/src/utils/Request.js
@@ -45,30 +45,3 @@ class Request {
 }
 
 export default Request;
-
-// export async function fetchPokemon() {
-//   const result = await fetch(url);
-//   const data = await result.json();
-//   return data;
-// }
-
-// export async function fetchPokemonList(url) {
-//   const result = await fetch(url);
-//   const data = await result.json();
-
-//   return data;
-// }
-
-// export async function fetchPokemonDetail(id) {
-//   const result = await fetch(url + `/${id}/`);
-//   const data = await result.json();
-
-//   return data;
-// }
-
-// export async function fetchPokemonEvolution(id) {
-//   const result = await fetch(url2 + `/${id}/`);
-//   const data = await result.json();
-
-//   return data;
-// }
