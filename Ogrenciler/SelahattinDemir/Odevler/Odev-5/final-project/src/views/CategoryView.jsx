3bd14e39b61922b26659093fa5bd07034e72cc6b
Selahattin Demir <selahattindemir686@gmail.com>
2023-06-26 22:05:37 +0300

    updated file

--- Ogrenciler/SelahattinDemir/Odevler/Odev-5/final-project/src/views/CategoryView.jsx
+++ Ogrenciler/SelahattinDemir/Odevler/Odev-5/final-project/src/views/CategoryView.jsx
@@ -1,15 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchProducts } from "../redux/slice/productsSlice";
 import Filter from "../components/Filter/Filter";
+import SortFilter from "../components/Filter/SortFilter";
 import Card from "../components/Card/Card";
 import { FilterTitle, ProductsLength } from "../components/Filter/FilterStyle";
 
 function CategoryView() {
   const dispatch = useDispatch();
   const [selectedCategories, setSelectedCategories] = useState([]);
+  const [sortBy, setSortBy] = useState("default");
 
   const { products } = useSelector((state) => state.products);
+  const isLoggedIn = useSelector((state) => state.loggedIn);
+
+  console.log("isLoggedIn", isLoggedIn);
 
   useEffect(() => {
     dispatch(fetchProducts());
@@ -17,30 +22,47 @@ function CategoryView() {
 
   const handleCategoryFilter = (category) => {
     if (selectedCategories.includes(category)) {
-      // Eğer kategori zaten seçili ise, seçimden kaldır
       setSelectedCategories(
         selectedCategories.filter((cat) => cat !== category)
       );
     } else {
-      // Değilse, seçime ekle
       setSelectedCategories([...selectedCategories, category]);
     }
   };
 
-  // Filtrelenmiş ürünleri almak için bir fonksiyon oluşturalım
   const getFilteredProducts = () => {
     if (selectedCategories.length === 0) {
-      // Hiçbir kategori seçilmediyse, tüm ürünleri döndür
-      return products;
+      return products.slice();
     } else {
-      // Seçilen kategorilere göre filtreleme yap
-      return products.filter((item) =>
-        selectedCategories.includes(item.category)
-      );
+      return products
+        .filter((item) => selectedCategories.includes(item.category))
+        .slice();
     }
   };
 
+  const handleSortChange = (e) => {
+    const sortOption = e.target.value;
+    setSortBy(sortOption);
+  };
+
   const filteredProducts = getFilteredProducts();
+
+  const sortedProducts = useMemo(() => {
+    const sortedCopy = [...filteredProducts];
+
+    if (sortBy === "price-asc") {
+      sortedCopy.sort((a, b) => a.price - b.price);
+    } else if (sortBy === "price-desc") {
+      sortedCopy.sort((a, b) => b.price - a.price);
+    } else if (sortBy === "name-asc") {
+      sortedCopy.sort((a, b) => a.title.localeCompare(b.title));
+    } else if (sortBy === "name-desc") {
+      sortedCopy.sort((a, b) => b.title.localeCompare(a.title));
+    }
+
+    return sortedCopy;
+  }, [filteredProducts, sortBy]);
+
   return (
     <div className="container">
       <div className="row">
@@ -53,12 +75,15 @@ function CategoryView() {
           />
         </div>
         <div className="col-md-9">
-          <div className="d-flex gap-2 align-items-center">
-            <FilterTitle>Products</FilterTitle>
-            <ProductsLength>{products.length} products</ProductsLength>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="d-flex gap-2 align-items-center">
+              <FilterTitle>Products</FilterTitle>
+              <ProductsLength>{sortedProducts.length} products</ProductsLength>
+            </div>
+            <SortFilter handleSortChange={handleSortChange} />
           </div>
           <div className="row">
-            {filteredProducts.map((item) => (
+            {sortedProducts.map((item) => (
               <Card key={item.id} data={item} />
             ))}
           </div>
