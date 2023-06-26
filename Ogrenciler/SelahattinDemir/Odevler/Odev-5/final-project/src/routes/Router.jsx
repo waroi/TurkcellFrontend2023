0984c05b69c87d2bebf2931ccc7eaba4982cc3ff
Selahattin Demir <selahattindemir686@gmail.com>
2023-06-26 21:53:17 +0300

    done user authentication

--- Ogrenciler/SelahattinDemir/Odevler/Odev-5/final-project/src/routes/Router.jsx
+++ Ogrenciler/SelahattinDemir/Odevler/Odev-5/final-project/src/routes/Router.jsx
@@ -1,12 +1,24 @@
+import { useEffect } from "react";
 import { useRoutes } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUsersLogin } from "../redux/slice/loginSlice";
 import RegisterView from "../views/RegisterView";
 import LoginView from "../views/LoginView";
 import HomeView from "../views/HomeView";
 import DetailView from "../views/DetailView";
-import Category from "../views/CategoryView";
+import CategoryView from "../views/CategoryView";
+import CartsView from "../views/CartsView";
 import NotFound from "../views/NotFound";
+import UnauthorizedAccess from "../utils/UnauthorizedAccess";
 
 const Router = () => {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.login.loggedIn);
+
+  useEffect(() => {
+    dispatch(fetchUsersLogin());
+  }, [dispatch]);
+
   const routes = useRoutes([
     {
       path: "/register",
@@ -22,11 +34,15 @@ const Router = () => {
     },
     {
       path: "/:id",
-      element: <DetailView />,
+      element: isLoggedIn ? <DetailView /> : <UnauthorizedAccess />,
     },
     {
       path: "/category",
-      element: <Category />,
+      element: isLoggedIn ? <CategoryView /> : <UnauthorizedAccess />,
+    },
+    {
+      path: "/carts",
+      element: isLoggedIn ? <CartsView /> : <UnauthorizedAccess />,
     },
     {
       path: "*",
