bef42c4049080d5c7f53d326ca477f856b814290
Selahattin Demir <selahattindemir686@gmail.com>
2023-06-27 23:34:08 +0300

    updated file

--- Ogrenciler/SelahattinDemir/Odevler/Odev-5/final-project/src/components/Navbar/Navbar.jsx
+++ Ogrenciler/SelahattinDemir/Odevler/Odev-5/final-project/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,39 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../../redux/slice/loginSlice";
 import { Ul, NavInput, IconWrapper, NavSpan, Nav } from "./NavbarStyle.js";
+import Request from "../../utils/Request";
 
 function Navbar() {
+  const request = new Request("http://localhost:3004/users");
   const dispatch = useDispatch();
   const isLoggedIn = useSelector((state) => state.login.loggedIn);
-  const users = localStorage.getItem("user");
-  const user = JSON.parse(users);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("user")));
+  }, []);
+
+  console.log(JSON.parse(localStorage.getItem("user")));
+
+  // const users = localStorage.getItem("user");
+  // const user = JSON.parse(users);
 
   const handleLogout = () => {
+    const updatedUser = {
+      ...user,
+      login: false,
+    };
+    request.put(user.id, updatedUser);
+
     dispatch(logout());
     localStorage.removeItem("user");
+
+    setUser(updatedUser);
   };
+
+  console.log(user);
 
   return (
     <div>
@@ -74,10 +95,15 @@ function Navbar() {
                 aria-label="Search"
               />
             </form>
-            <div className="d-flex gap-2">
+            <div className="d-flex gap-2 align-items-center ">
               {isLoggedIn ? (
-                <Link to="/carts" className="btn-nav" type="submit">
-                  <i className="bi bi-cart-check"></i>
+                <Link
+                  to={`/carts/${user.id}`}
+                  className="btn-nav"
+                  type="submit"
+                >
+                  <i className="bi bi-cart-check fs-4"></i>
+                  <span className="badge bg-success">{user.carts.length}</span>
                 </Link>
               ) : (
                 <Link to="/login" className="btn-nav me-3" type="submit">
@@ -93,7 +119,7 @@ function Navbar() {
                     data-bs-toggle="dropdown"
                     aria-expanded="false"
                   >
-                    <i className="bi bi-person-circle"></i>
+                    <i className="bi bi-person-circle fs-4"></i>
                   </button>
                   <ul className="dropdown-menu border-0 w-auto ">
                     <li>
