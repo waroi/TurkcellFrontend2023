bf6dbaeae99e2e45295fbaf3f9ecfb0a80a4e7f3
Selahattin Demir <selahattindemir686@gmail.com>
2023-06-30 16:25:25 +0300

    added  handleAddToCart function

--- Ogrenciler/SelahattinDemir/Odevler/Odev-5/final-project/src/components/Card/Card.jsx
+++ Ogrenciler/SelahattinDemir/Odevler/Odev-5/final-project/src/components/Card/Card.jsx
@@ -1,5 +1,7 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import {
   CardContainer,
   CardImage,
@@ -7,6 +9,7 @@ import {
   Description,
   ProductInfo,
 } from "./CardStyle.js";
+import Request from "../../utils/Request";
 
 function truncateText(text, maxLength) {
   if (text.length <= maxLength) {
@@ -16,8 +19,76 @@ function truncateText(text, maxLength) {
 }
 
 function Card({ data }) {
+  const request = new Request("http://localhost:3004/users");
+  const [user, setUser] = useState(null);
   const truncatedTitle = truncateText(data.title, 35);
   const truncatedDescription = truncateText(data.description, 55);
+  const availableStock = data.rating?.count || 0;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await request.get();
+        const loggedInUser = response.find((user) => user.login === true);
+        setUser(loggedInUser);
+      } catch (error) {
+        toast.error("Error fetching user.");
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  const handleAddToCart = () => {
+    const existingItem = user.carts.find((item) => item.id === data.id);
+
+    if (existingItem) {
+      if (existingItem.quantity >= availableStock) {
+        toast.error("Insufficient stock. You cannot add more of this item.");
+        return;
+      }
+
+      const updatedCarts = user.carts.map((item) => {
+        if (item.id === data.id) {
+          return {
+            ...item,
+            quantity: item.quantity + 1,
+          };
+        }
+
+        return item;
+      });
+
+      const updatedUser = {
+        ...user,
+        carts: updatedCarts,
+      };
+
+      request.put(user.id, updatedUser);
+      setUser(updatedUser);
+      toast.success("Item added to cart.");
+      return;
+    } else if (availableStock < 1) {
+      toast.error("Insufficient stock. You cannot add this item to cart.");
+      return;
+    }
+
+    const updatedUser = {
+      ...user,
+      carts: [
+        ...user.carts,
+        {
+          ...data,
+          quantity: 1,
+        },
+      ],
+    };
+
+    request.put(user.id, updatedUser);
+    setUser(updatedUser);
+    toast.success("Item added to cart.");
+  };
+
   return (
     <CardContainer className="col-lg-3 col-md-4">
       <div className="card overflow-hidden border-0">
@@ -56,9 +127,13 @@ function Card({ data }) {
             <Link to={`/${data.id}`} className="btn btn-primary">
               Read More
             </Link>
-            <a href="#" className="btn btn-danger">
-              Add to Cart
-            </a>
+            <button
+              onClick={handleAddToCart}
+              className="btn btn-danger"
+              disabled={availableStock < 1}
+            >
+              {availableStock < 1 ? "Out of Stock" : "Add to Cart"}
+            </button>
           </div>
         </div>
       </div>
