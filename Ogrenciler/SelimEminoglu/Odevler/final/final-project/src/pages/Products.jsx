4e016c68372f75e3e3eff4243fe954fadff1ed2b
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-06-30 10:54:31 +0300

    ProductPage Güncellemesi
    Products sayfası için ads eklendi.

--- Ogrenciler/SelimEminoglu/Odevler/final/final-project/src/pages/Products.jsx
+++ Ogrenciler/SelimEminoglu/Odevler/final/final-project/src/pages/Products.jsx
@@ -1,10 +1,12 @@
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
+import ProductAds from "../components/ProductAds/ProductAds";
 
 function Products() {
   return (
     <div>
       <Navbar />
+      <ProductAds />
       <Footer />
     </div>
   );
