2f41220267cfa0689798c17489ec20781b587538
Ege <karaege98@gmail.com>
2023-07-01 20:24:08 +0300

    Update StyledSortArea.js

--- Ogrenciler/Ege/odevler/final/e-commerce/src/components/ProductList/SortArea/StyledSortArea.js
+++ Ogrenciler/Ege/odevler/final/e-commerce/src/components/ProductList/SortArea/StyledSortArea.js
@@ -10,6 +10,19 @@ const StyledSortArea = styled.div`
     font-size: 24px;
     font-weight: 700;
   }
+
+  select {
+    padding: 6px 10px 4px 20px;
+    align-items: center;
+    gap: 8px;
+    border-radius: 20px;
+    border: 1px solid #ccd1d2;
+    color: #667479;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 20px;
+    text-transform: capitalize;
+  }
 `;
 
 export default StyledSortArea;
