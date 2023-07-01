cc30658fb14a864d39507e06fc158fa0204eedeb
Ege <karaege98@gmail.com>
2023-07-01 18:14:42 +0300

    account dropdown fixed

--- Ogrenciler/Ege/odevler/final/e-commerce/src/components/Navbar/AccountDropdown/AccountDropdown.jsx
+++ Ogrenciler/Ege/odevler/final/e-commerce/src/components/Navbar/AccountDropdown/AccountDropdown.jsx
@@ -2,27 +2,41 @@ import { useDispatch, useSelector } from "react-redux"
 import { setUser } from "../../../redux/slices/userSlice"
 import ButtonPrimary from "../../../styledComponents/ButtonPrimary"
 import StyledAccountDropdown from "./StyledAccountDropdown"
-import dots from "../../../assets/Dots.svg"
+import caretDown from "../../../assets/Caret_Down_Dark.svg"
+import { useState } from "react"
 const AccountDropdown = () => {
     const user = useSelector(state => state.user.user)
-    const dispatch = useDispatch()
-    return (
-        <StyledAccountDropdown className="dropdown">
-            <div className="d-flex align-items-center gap-3 dropdownButton" data-bs-toggle="dropdown" aria-expanded="false">
-                <img className="profileImage" src={user.imgURL} alt="userImage" />
-                <p>{user.name} <img className="dots" src={dots} alt="dots" /></p>
+    const [isOpen, setIsOpen] = useState(false);
+    const dispatch = useDispatch();
 
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
+        console.log("first")
+        console.log(isOpen)
+    };
+
+    const handleLogout = () => {
+        dispatch(setUser(null));
+        localStorage.removeItem("userData");
+    };
+    return (
+        <StyledAccountDropdown >
+            <div className="d-flex align-items-center gap-3 dropdownButton" onClick={handleToggle}>
+                <img className="profileImage" src={user.imgURL} alt="userImage" />
+                <p className="m-0 profileText d-flex align-items-center">
+                    {user.name} <img className="caretDown" src={caretDown} alt="caretDown" />
+                </p>
             </div>
-            <ul className="dropdown-menu">
-                <li className="px-3"><ButtonPrimary
-                    className="w-100"
-                    onClick={() => {
-                        dispatch(setUser(null))
-                        localStorage.removeItem("userData")
-                    }}
-                >Logout</ButtonPrimary></li>
-            </ul>
-        </StyledAccountDropdown >
+            {isOpen && (
+                <ul className="dropdownMenu">
+                    <li className="px-3">
+                        <ButtonPrimary className="w-100" onClick={handleLogout}>
+                            Logout
+                        </ButtonPrimary>
+                    </li>
+                </ul>
+            )}
+        </StyledAccountDropdown>
     )
 }
 
