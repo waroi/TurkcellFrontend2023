c5cc61771a807e87b5a5fef43987746c1a71364e
Ege <karaege98@gmail.com>
2023-07-01 12:54:48 +0300

    account dropdown added

--- Ogrenciler/Ege/odevler/final/e-commerce/src/components/Navbar/AccountDropdown/AccountDropdown.jsx
+++ Ogrenciler/Ege/odevler/final/e-commerce/src/components/Navbar/AccountDropdown/AccountDropdown.jsx
@@ -1,17 +1,28 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { setUser } from "../../../redux/slices/userSlice"
 import ButtonPrimary from "../../../styledComponents/ButtonPrimary"
+import StyledAccountDropdown from "./StyledAccountDropdown"
+import dots from "../../../assets/Dots.svg"
 const AccountDropdown = () => {
+    const user = useSelector(state => state.user.user)
     const dispatch = useDispatch()
     return (
-        <div>
-            <ButtonPrimary onClick={() => {
-                dispatch(setUser(null))
-                localStorage.removeItem("userData")
-            }}>
-                Logout
-            </ButtonPrimary>
-        </div>
+        <StyledAccountDropdown className="dropdown">
+            <div className="d-flex align-items-center gap-3 dropdownButton" data-bs-toggle="dropdown" aria-expanded="false">
+                <img className="profileImage" src={user.imgURL} alt="userImage" />
+                <p>{user.name} <img className="dots" src={dots} alt="dots" /></p>
+
+            </div>
+            <ul className="dropdown-menu">
+                <li className="px-3"><ButtonPrimary
+                    className="w-100"
+                    onClick={() => {
+                        dispatch(setUser(null))
+                        localStorage.removeItem("userData")
+                    }}
+                >Logout</ButtonPrimary></li>
+            </ul>
+        </StyledAccountDropdown >
     )
 }
 
---
+++ Ogrenciler/Ege/odevler/final/e-commerce/src/components/Navbar/AccountDropdown/StyledAccountDropdown.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledAccountDropdown = styled.div`
+  .dropdownButton {
+    .profileImage {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+    }
+  }
+`;
+
+export default StyledAccountDropdown;
