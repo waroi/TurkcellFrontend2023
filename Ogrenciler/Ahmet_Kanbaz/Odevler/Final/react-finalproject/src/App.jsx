7b54b4f124d5ce86e4504c5e89447fd02d5c3d2b
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2023-07-02 14:20:09 +0300

    React-Final-Project-Update

--- Ogrenciler/Ahmet_Kanbaz/Odevler/Final/react-finalproject/src/App.jsx
+++ Ogrenciler/Ahmet_Kanbaz/Odevler/Final/react-finalproject/src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { GlobalStyle } from "./GlobalStyle/GlobalStyle";
+import Router from "./routes/Router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllProducts, fetchAllUsers } from "./utils/request";
+import { setFilteredProducts } from "./redux/slices/filterSlice/filterSlice";
+import { setCart } from "./redux/slices/cartSlice/cartSlice";
+import { setUser } from "./redux/slices/usersSlice/userSlice";
+import {fetchLoginUser} from './utils/request'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
+  const allProducts = useSelector((state) => state.products.products);
+  const allUsers = useSelector((state) => state.user.allUsers);
+  
+  const getSingleUserFromLocaleStorage = async (id) => {
+    const response = await fetchLoginUser(id);
+    dispatch(setCart(response.cart));
+    localStorage.setItem("isLogin", JSON.stringify(response));
+  }
 
+  useEffect(() => {
+    if (allProducts.length === 0) {
+      dispatch(fetchAllProducts());
+    }
+    dispatch(setFilteredProducts(allProducts));
+    if (allUsers.length === 0) {
+      dispatch(fetchAllUsers());
+    }
+    const user = localStorage.getItem("isLogin");
+    if (user) {
+      const parsedUser = JSON.parse(user);
+      dispatch(setUser(parsedUser));
+      getSingleUserFromLocaleStorage(parsedUser.id)
+    }
+
+  }, [allProducts]);
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GlobalStyle />
+      <Router />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
